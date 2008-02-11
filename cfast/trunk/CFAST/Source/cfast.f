1d91c4818ec960ca881d038500f021d97cc48525
cfastdev <cfastdev@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2008-02-11 19:46:34 +0000

    Fixed issue #17 where species spreadsheet output headers did not match data columns.

--- cfast/trunk/CFAST/Source/cfast.f
+++ cfast/trunk/CFAST/Source/cfast.f
@@ -157,7 +157,7 @@
       include "precis.fi"
       include "cfast.fi"
 
-      DATA CRDATE/2008,1,23/
+      DATA CRDATE/2008,2,11/
       END
       
       SUBROUTINE INITSOLN(T,PDOLD,PDZERO,RPAR,IPAR)

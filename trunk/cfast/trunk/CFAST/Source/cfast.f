768c5d7abdeeb244fd3eacaccb6fe41cfb35790e
cfastdev <cfastdev@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2009-03-12 13:58:10 +0000

    Plume temperature calculation updated to be consistent with all three possible combinations (fire and target in lower layer, both in upper layer or split between the layers). All three use Heskestad's correlation for now.  Will try McCaffrey's correlation too and see which provides better comparisons with validation data.

--- trunk/cfast/trunk/CFAST/Source/cfast.f
+++ trunk/cfast/trunk/CFAST/Source/cfast.f
@@ -140,7 +140,7 @@
      . outputformat /0/, IOFILO/6/, REPORTO/0/, IOFILI/1/, LOGERR/3/,
      . OBFILI/99/, AHFILO/98/, WEBFILE/97/, trace/.false./
     
-     . VERSION/6101/
+     . VERSION/6200/
 ! 
       DATA IDEF /1/, ITODEF /2/, LAYDEF /1/, ISPDEF /3/, ICHRS /4/,
      . IDEVO /6/, IDEVC /1/, SETAXP /.FALSE./, OPENPC /.FALSE./,
@@ -157,7 +157,7 @@
       include "precis.fi"
       include "cfast.fi"
 
-      DATA CRDATE/2008,10,28/
+      DATA CRDATE/2009,3,4/
       END
       
       SUBROUTINE INITSOLN(T,PDOLD,PDZERO,RPAR,IPAR)

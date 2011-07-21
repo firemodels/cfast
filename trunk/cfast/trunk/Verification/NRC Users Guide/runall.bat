09837bb4cda57d7e2e7aed6b2e49c77f0974b355
cfastdev <cfastdev@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2011-07-21 16:06:11 +0000

    Updated NRC Users Guide examples to match latest calculations

--- trunk/cfast/trunk/Verification/NRC Users Guide/runall.bat
+++ trunk/cfast/trunk/Verification/NRC Users Guide/runall.bat
@@ -1,32 +1,40 @@
-path C:\Documents and Settings\Administrator\My Documents\Visual Studio 2005\Projects\cfast\Bin\; %PATH%
 echo off
 call cleanall.bat
 echo NRC Users Guide Test Cases
 cd "1 - Cabinet Fire in MCR"
 echo 1 - Cabinet Fire in MCR
-cfast "Cabinet fire in MCR" 
+"..\..\..\Visual Studio 2010\Projects\cfast\Bin\CFAST.exe" "Cabinet fire in MCR" /V 
+echo 1 - Cabinet Fire in MCR No Ventilation
+"..\..\..\Visual Studio 2010\Projects\cfast\Bin\CFAST.exe" "Cabinet fire in MCR No Ventilation" /V 
 cd ..\
 cd "2 - Cabinet Fire in Switchgear"
 echo 2 - Cabinet Fire in Switchgear
-cfast "Cabinet fire in Switchgear" 
+"..\..\..\Visual Studio 2010\Projects\cfast\Bin\CFAST.exe" "Initial Fire Only" /V
+"..\..\..\Visual Studio 2010\Projects\cfast\Bin\CFAST.exe" "Cabinet fire in Switchgear" /V
+"..\..\..\Visual Studio 2010\Projects\cfast\Bin\CFAST.exe" "Combined Fires" /V
 cd ..\
 cd "3 - Lube Oil Fire in Pump Room"
-echo 3 - Lube Oil Fire in Pump Room
-cfast "Oil Fire in Pump Room"
+rem echo 3 - Lube Oil Fire in Pump Room
+rem "..\..\..\Visual Studio 2010\Projects\cfast\Bin\CFAST.exe" "Oil Fire in Pump Room" /V
 cd ..\
 cd "4 - MCC Fire in Switchgear"
 echo 4 - MCC Fire in Switchgear
-cfast "MCC in Switchgear"
+"..\..\..\Visual Studio 2010\Projects\cfast\Bin\CFAST.exe" "MCC in Switchgear" /V
 cd ..\
 cd "5 - Trash Fire in Cable Spreading Room"
 echo 5 - Trash Fire in Cable Spreading Room
-cfast "Trash Fire in Cable Spreading Room" 
+"..\..\..\Visual Studio 2010\Projects\cfast\Bin\CFAST.exe" "Trash Fire in Cable Spreading Room" /V
 cd ..\
 cd "6 - Oil Fire in Turbine Building"
-echo 6 - Oil Fire in Turbine Building
-cfast "Oil in Turbine Building"
+rem echo 6 - Oil Fire in Turbine Building
+rem "..\..\..\Visual Studio 2010\Projects\cfast\Bin\CFAST.exe" "Oil in Turbine Building" /V
 cd ..\
 cd "7 - Transient Fire in Corridor"
-echo 7 - Transient Fire in Corridor
-cfast "Transient in Corridor"
+rem echo 7 - Transient Fire in Corridor
+rem "..\..\..\Visual Studio 2010\Projects\cfast\Bin\CFAST.exe" "Transient in Corridor" /V
 cd ..\
+cd "8 - Cable Fire in Annulus"
+echo 8 - Cable Fire in Annulus
+"..\..\..\Visual Studio 2010\Projects\cfast\Bin\CFAST.exe" "Cable in Annulus" /V
+cd ..\
+

6ae40b131eaad2ad345795bb3a81dfdf60b2e614
cfastdev <cfastdev@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2012-06-20 14:04:45 +0000

    cfast source:  New modules files for updating to use statements

---
+++ trunk/cfast/trunk/CFAST/Include/cfast_data.f90
@@ -0,0 +1,19 @@
+module  iofiles
+
+    implicit none
+    
+!File descriptors for cfast
+
+    character(6), parameter :: heading="VERSN"
+	character(64) ::  project
+	character(256) :: datapath, exepath, inputfile, outputfile, smvhead, smvdata, smvcsv, &
+	      ssflow, ssnormal, ssspecies, sswall, errorlogging, stopfile, solverini, &
+	      historyfile, queryfile, statusfile, kernelisrunning
+
+! Work arrays for the csv input routines
+
+    integer, parameter :: nrow=200, ncol=200
+    real*8 rarray(nrow,ncol) 
+    character(128) :: carray(nrow,ncol)
+
+end module iofiles

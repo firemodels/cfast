a9a8e2c245f77170313d702476b87a6b8dbc4eb4
cfastdev <cfastdev@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2012-08-15 12:29:52 +0000

    Validation: added all of the LLNL tests to the validation suite.

--- trunk/cfast/trunk/Validation/runall.bat
+++ trunk/cfast/trunk/Validation/runall.bat
@@ -1,5 +1,154 @@
 call cleanall.bat
 echo.| time
+echo LLNL Tests
+cd LLNL_Enclosure
+cd LLNL_01
+..\..\..\bin\cfast LLNL_01 /V
+cd ..
+cd LLNL_02
+..\..\..\bin\cfast LLNL_02 /V
+cd ..
+cd LLNL_03
+..\..\..\bin\cfast LLNL_03 /V
+cd ..
+cd LLNL_04
+..\..\..\bin\cfast LLNL_04 /V
+cd ..
+cd LLNL_05
+..\..\..\bin\cfast LLNL_05 /V
+cd ..
+cd LLNL_06
+..\..\..\bin\cfast LLNL_06 /V
+cd ..
+cd LLNL_07
+..\..\..\bin\cfast LLNL_07 /V
+cd ..
+cd LLNL_08
+..\..\..\bin\cfast LLNL_08 /V
+cd ..
+cd LLNL_09
+..\..\..\bin\cfast LLNL_09 /V
+cd ..
+cd LLNL_10
+..\..\..\bin\cfast LLNL_10 /V
+cd ..
+cd LLNL_11
+..\..\..\bin\cfast LLNL_11 /V
+cd ..
+cd LLNL_12
+..\..\..\bin\cfast LLNL_12 /V
+cd ..
+cd LLNL_13
+..\..\..\bin\cfast LLNL_13 /V
+cd ..
+cd LLNL_14
+..\..\..\bin\cfast LLNL_14 /V
+cd ..
+cd LLNL_18
+..\..\..\bin\cfast LLNL_18 /V
+cd ..
+cd LLNL_19
+..\..\..\bin\cfast LLNL_19 /V
+cd ..
+cd LLNL_21
+..\..\..\bin\cfast LLNL_21 /V
+cd ..
+cd LLNL_22
+..\..\..\bin\cfast LLNL_22 /V
+cd ..
+cd LLNL_23
+..\..\..\bin\cfast LLNL_23 /V
+cd ..
+cd LLNL_24
+..\..\..\bin\cfast LLNL_24 /V
+cd ..
+cd LLNL_27
+..\..\..\bin\cfast LLNL_27 /V
+cd ..
+cd LLNL_28
+..\..\..\bin\cfast LLNL_28 /V
+cd ..
+cd LLNL_29
+..\..\..\bin\cfast LLNL_29 /V
+cd ..
+cd LLNL_30
+..\..\..\bin\cfast LLNL_30 /V
+cd ..
+cd LLNL_31
+..\..\..\bin\cfast LLNL_31 /V
+cd ..
+cd LLNL_32
+..\..\..\bin\cfast LLNL_32 /V
+cd ..
+cd LLNL_33
+..\..\..\bin\cfast LLNL_33 /V
+cd ..
+cd LLNL_34
+..\..\..\bin\cfast LLNL_34 /V
+cd ..
+cd LLNL_35
+..\..\..\bin\cfast LLNL_35 /V
+cd ..
+cd LLNL_36
+..\..\..\bin\cfast LLNL_36 /V
+cd ..
+cd LLNL_37
+..\..\..\bin\cfast LLNL_37 /V
+cd ..
+cd LLNL_38
+..\..\..\bin\cfast LLNL_38 /V
+cd ..
+cd LLNL_39
+..\..\..\bin\cfast LLNL_39 /V
+cd ..
+cd LLNL_41
+..\..\..\bin\cfast LLNL_41 /V
+cd ..
+cd LLNL_43
+..\..\..\bin\cfast LLNL_43 /V
+cd ..
+cd LLNL_44
+..\..\..\bin\cfast LLNL_44 /V
+cd ..
+cd LLNL_45
+..\..\..\bin\cfast LLNL_45 /V
+cd ..
+cd LLNL_46
+..\..\..\bin\cfast LLNL_46 /V
+cd ..
+cd LLNL_47
+..\..\..\bin\cfast LLNL_47 /V
+cd ..
+cd LLNL_49
+..\..\..\bin\cfast LLNL_49 /V
+cd ..
+cd LLNL_50
+..\..\..\bin\cfast LLNL_50 /V
+cd ..
+cd LLNL_51
+..\..\..\bin\cfast LLNL_51 /V
+cd ..
+cd LLNL_52
+..\..\..\bin\cfast LLNL_52 /V
+cd ..
+cd LLNL_59
+..\..\..\bin\cfast LLNL_59 /V
+cd ..
+cd LLNL_60
+..\..\..\bin\cfast LLNL_60 /V
+cd ..
+cd LLNL_61
+..\..\..\bin\cfast LLNL_61 /V
+cd ..
+cd LLNL_62
+..\..\..\bin\cfast LLNL_62 /V
+cd ..
+cd LLNL_63
+..\..\..\bin\cfast LLNL_63 /V
+cd ..
+cd LLNL_64
+..\..\..\bin\cfast LLNL_64 /V
+cd ..\..\
 echo NBS 1 room furniture tests 1, 6
 cd 1rfurn
 ..\..\bin\cfast 1rfurn1 /V
@@ -162,17 +311,5 @@ cd High_Bay
 ..\..\bin\cfast Keflavik_5 /V
 ..\..\bin\cfast Keflavik_6 /V
 ..\..\bin\cfast Keflavik_7 /V
-cd ..\
-echo LLNL Tests
-cd LLNL_Enclosure
-..\..\bin\cfast LLNL_01 /V
-..\..\bin\cfast LLNL_02 /V
-..\..\bin\cfast LLNL_03 /V
-..\..\bin\cfast LLNL_04 /V
-..\..\bin\cfast LLNL_05 /V
-..\..\bin\cfast LLNL_06 /V
-..\..\bin\cfast LLNL_07 /V
-..\..\bin\cfast LLNL_08 /V
-..\..\bin\cfast LLNL_09 /V
 cd ..\
 echo.| time

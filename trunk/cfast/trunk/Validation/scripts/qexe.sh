0cf5a51a48dd0b55b15a2d453667ea9d45e2f871
gforney <gforney@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2012-12-29 18:47:51 +0000

    vv scripts: update linux scripts to run cfast validation cases using specified queues

---
+++ trunk/cfast/trunk/Validation/scripts/qexe.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+# $Date: 2012-01-18 21:58:13 -0500 (Wed, 18 Jan 2012) $ 
+# $Revision: 9861 $
+# $Author: gforney $
+
+PROG=$0
+
+EXPECTED_ARGS=1
+
+# setup default queue name
+
+progname=qexe.sh
+queue=batch
+
+if [ $# -lt $EXPECTED_ARGS ]
+then
+  echo "Usage: $progname [-q queue] dir command command_arguments"
+  echo ""
+  echo "This script runs a program using the queing system"
+  echo ""
+  echo " -q queue - name of the queue. choices: [default: $queue (other choices:"  
+  echo "    vis, fire60s and fire70s)"
+  echo " command - full path to program to be run"
+  echo " command_arguments - arguments passed to command"
+  echo ""
+  exit
+fi
+
+# default parameter settings
+
+nprocesses=1
+nprocesses_per_node=1
+nprocesses_per_node_defined=0
+
+# read in parameters from command line
+
+while getopts 'q:' OPTION
+do
+case $OPTION  in
+  q)
+   queue="$OPTARG"
+  ;;
+esac
+done
+shift $(($OPTIND-1))
+
+# set number of processes per node  to 4 if the fire60s queue is being used
+# (the fire60s only have 4 cores)
+
+dir=$1
+command=$2
+in=$3
+infile=${in%.*}
+TITLE="$infile(cfast)"
+
+nnodes=1
+
+fulldir=`pwd`/$dir
+
+out=$fulldir/$infile.err
+outlog=$fulldir/$infile.log
+stopfile=$fulldir/$infile.stop
+
+# make sure files that are needed exist
+
+if ! [ -e $command ]; then
+  echo "The command, $command, does not exist. Run aborted."
+fi
+if [ -e $outlog ]; then
+  echo "Removing log file: $outlog"
+  rm $outlog
+fi
+if [ $STOPFDS ]; then
+ echo "stopping case: $in"
+ touch $stopfile
+ exit
+fi
+if [ -e $stopfile ]; then
+ rm $stopfile
+fi
+
+
+scriptfile=/tmp/script.$$
+cat << EOF > $scriptfile
+#!/bin/bash -f
+#PBS -N $TITLE
+#PBS -e $out
+#PBS -o $outlog
+#PBS -l nodes=$nnodes:ppn=$nprocesses_per_node
+#\$ -N $TITLE
+#\$ -e $out
+#\$ -o $outlog
+#\$ -l nodes=$nnodes:ppn=$nprocesses_per_node
+
+cd $fulldir
+echo Start time: \`date\`
+echo Running $infile on \`hostname\`
+echo Directory: \`pwd\`
+$command $in
+EOF
+chmod +x $scriptfile
+qsub -q $queue $scriptfile
+rm $scriptfile
--- trunk/cfast/trunk/Validation/scripts/runcfast2q.sh
+++ trunk/cfast/trunk/Validation/scripts/runcfast2q.sh
@@ -1,11 +1,24 @@
 #!/bin/bash -f
+
+queue=batch
+while getopts 'q:' OPTION
+do
+case $OPTION in
+  q)
+   queue="$OPTARG"
+   ;;
+esac
+#shift
+done
+shift $(($OPTIND-1))
+
 dir=$1
 dir2=$2
 infile=$3
 
 fulldir=$BASEDIR/$dir/$dir2
 
-echo -----------------------------
-echo running $infile in $fulldir
-$QEXE $dir/$dir2 $CFAST $infile 
-echo -----------------------------
+echo --------------------------------------------------
+echo running $infile in $fulldir using the $queue queue
+$QEXE -q $queue $dir/$dir2 $CFAST $infile 
+echo --------------------------------------------------

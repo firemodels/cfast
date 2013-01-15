31aa0d49f52f09229e5855244fced7ce216d4c27
cfastdev@gmail.com <cfastdev@gmail.com@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2013-01-15 21:19:03 +0000

    Validation: Trying to generalize the simple V&V calcs.  This is a work in progress.

--- trunk/cfast/trunk/VandV_Calcs/VandV_Calcs.f90
+++ trunk/cfast/trunk/VandV_Calcs/VandV_Calcs.f90
@@ -1,22 +1,24 @@
     program VandV_Calcs
 
-        implicit none
+    implicit none
 
     ! Variables
-    integer, parameter :: ntests = 2000, nrow = 10000, ncol = 500, list_nrow = 1000, list_ncol = 100, test_cols = 3*ntests
-    logical, parameter :: write_data = .false.
+    integer, parameter :: ntests = 2000, nrow = 10000, ncol = 500, list_nrow = 1000, list_ncol = 100
 
-    character :: base_folder*128, comparelist_file*128, partial_filename*128, metric*3, test_column_names(ntests,3)*50, all_data_row_values(test_cols)*15
-    character :: list_carray(list_nrow, list_ncol)*128, model_carray(nrow,ncol)*50, switch_id*3, quantity*50
+    character :: base_folder*128, comparelist_file*128, partial_filename*128
+    character :: list_carray(list_nrow, list_ncol)*128, model_carray(nrow,ncol)*50, switch_id*3
 
-    integer experimental_numr, experimental_numc, list_numr, list_numc, model_numr, model_numc, find_column, io_error, ir, ic, it, i, ntest, test_row_max, ir_test, test_length(ntests), row_number(ntests)
+    integer list_numr, list_numc, model_numr, model_numc, find_column, io_error, ir, ic, it, i, ntest
     integer :: switch_id_column
-    integer :: d2_filename_column, d2_col_name_row_column, d2_ind_col_name_column, d2_data_row_column, d2_dep_col_name_column, d2_comp_start_column, d2_comp_end_column, d2_initial_value_column
+    integer :: d2_filename_column, d2_col_name_row_column, d2_ind_col_name_column, d2_data_row_column, d2_data_col_name_column, d2_data_column_count_column
 
-    character :: d2_filename*128, d2_ind_col_name*128, d2_dep_col_names*128, d2_dep_col_name*128
-    integer :: d2_col_name_row, d2_data_row, d2_ind_data_col, d2_dep_data_col, d2x_len, d2y_len
+    character :: d2_filename*128, d2_ind_col_name*128, d2_data_col_names(list_ncol)*128, d2_data_col_name
+    integer :: d2_col_name_row, d2_data_row, d2_ind_data_col, d2_data_data_col(list_ncol), d2_data_column_count, d2x_len, d2y_len(list_ncol)
 
-    real :: list_rarray(list_nrow, list_ncol), model_rarray(nrow,ncol), d2x(nrow), d2y(nrow), all_data(ntests,nrow,4), d2_initial_value, activation_time
+    real :: list_rarray(list_nrow, list_ncol), model_rarray(nrow,ncol), d2x(nrow), d2y(nrow,list_ncol)
+
+    ! Calculation specific variables
+    real :: activation_time
 
     ! Body of ModelVandV
     base_folder = '..\..\cfast\Validation\'
@@ -33,53 +35,46 @@
     d2_col_name_row_column = find_column(list_carray,list_nrow,list_ncol,1,'d2_Col_Name_Row')
     d2_ind_col_name_column = find_column(list_carray,list_nrow,list_ncol,1,'d2_Ind_Col_Name')
     d2_data_row_column =  find_column(list_carray,list_nrow,list_ncol,1,'d2_Data_Row')
-    d2_dep_col_name_column = find_column(list_carray,list_nrow,list_ncol,1,'d2_Dep_Col_Name')
+    d2_data_column_count_column = find_column(list_carray,list_nrow,list_ncol,1,'d2_Data_Column_Count')
+    d2_data_col_name_column = find_column(list_carray,list_nrow,list_ncol,1,'d2_Dep_Col_Name')
 
     ir=0
     ntest = 0
-    test_row_max = 0
 10  ir = ir + 1
     switch_id = list_carray(ir,switch_id_column)
     if (switch_id=='d') then
 
-        ! read in the model data   
-        d2_filename = trim(base_folder) // list_carray(ir,d2_filename_column)
-        write (*,*) ir,trim(d2_filename)
-        open (unit=8,file=d2_filename,form='formatted',action='read',iostat=io_error)
-        call readcsv(8,model_rarray,model_carray,nrow,ncol,1,model_numr, model_numc)
-        close (unit=8)
-        
-        d2_col_name_row = list_rarray(ir,d2_col_name_row_column)
-        d2_ind_col_name = list_carray(ir,d2_ind_col_name_column)
-        d2_ind_data_col = find_column(model_carray,nrow,ncol,d2_col_name_row,d2_ind_col_name)
-        d2_dep_col_names = list_carray(ir,d2_dep_col_name_column)
-        d2_data_row = list_rarray(ir,d2_data_row_column)
+    ! read in the model data   
+    d2_filename = trim(base_folder) // list_carray(ir,d2_filename_column)
+    write (*,*) ir,trim(d2_filename)
+    open (unit=8,file=d2_filename,form='formatted',action='read',iostat=io_error)
+    call readcsv(8,model_rarray,model_carray,nrow,ncol,1,model_numr, model_numc)
+    close (unit=8)
 
-        ! find dependent column(s) that match the independent columns
-        do while (len_trim(d2_dep_col_names)>0)
-            if(index(d2_dep_col_names,'|')>0) then
-                d2_dep_col_name = d2_dep_col_names(1:(index(d2_dep_col_names,'|')-1))
-                d2_dep_col_names = d2_dep_col_names((index(d2_dep_col_names,'|')+1):)
-            else
-                d2_dep_col_name = d2_dep_col_names
-                d2_dep_col_names=''
-            end if
-            d2_dep_data_col = find_column(model_carray,nrow,ncol,d2_col_name_row,d2_dep_col_name)
+    d2_col_name_row = list_rarray(ir,d2_col_name_row_column)
+    d2_ind_col_name = list_carray(ir,d2_ind_col_name_column)
+    d2_ind_data_col = find_column(model_carray,nrow,ncol,d2_col_name_row,d2_ind_col_name)
+    d2_data_col_names = list_carray(ir,d2_data_col_name_column)
+    d2_data_row = list_rarray(ir,d2_data_row_column)
+    d2_data_column_count = list_rarray(ir,d2_data_column_count_column)
 
-            if (d2_ind_data_col==0.or.d2_dep_data_col==0) then
-                write (*,*) 'Data column error =',d2_ind_data_col,d2_dep_data_col
-                stop
-            end if
+    ! find dependent column(s) that match the independent columns
 
-            ! Get the four vectors for this comparison ... d1x and d1y are the experimental values; d2x and d2y are the model values
-            call load_vector(model_rarray,model_carray,nrow,model_numr,d2_ind_data_col,d2_data_row,d2x,d2x_len)
-            call load_vector(model_rarray,model_carray,nrow,model_numr,d2_dep_data_col,d2_data_row,d2y,d2y_len)
+    ! Get the vectors for this calculation ...d2x and d2y(i)
+    call load_vector(model_rarray,model_carray,nrow,model_numr,d2_ind_data_col,d2_data_row,d2x,d2x_len)
 
+    do while (len_trim(d2_data_col_names)>0)
+        do ic = 1,d2_data_column_count
+            call find_column_name(d2_data_col_names(ic),d2_data_column_name)
+            d2_data_data_col(ic) = find_column(model_carray,nrow,ncol,d2_col_name_row,d2_data_col_name) 
+            call load_vector(model_rarray,model_carray,nrow,model_numr,d2_data_data_col(ic),d2_data_row,d2y(ic),d2y_len(ic))
+        end do
+
+        if (do_activation_time) then
             ! Calculations of the activation times go here using values of d2x, d2y (model values)
-            
-            if (d2x_len==d2y_len) then
+            if (d2x_len==d2y_len(1)) then
                 do i = 1, d2x_len
-                    if (d2y(i)/=0.0) then
+                    if (d2y(i,1)/=0.0) then
                         activation_time = d2x(i)
                         exit
                     end if
@@ -88,39 +83,15 @@
                 write (*,*) 'Data error, x and y lengths are not equal', d2x_len, d2y_len
                 stop
             end if
-                
+
             partial_filename = d2_filename
             if (len_trim(d2_filename)>20) partial_filename = '...' // d2_filename(len_trim(d2_filename)-20:)
-            write (9,'(i4,3x,a25,3x,a15,f12.3)') ir, trim(partial_filename), trim(d2_dep_col_name), activation_time
-        end do
-    end if
+            write (9,'(i4,3x,a25,3x,a15,f12.3)') ir, trim(partial_filename), trim(d2_data_col_name), activation_time
+        end if
+    end do
 
     if (ir.le.list_numr.and.list_carray(ir,1).ne.'End') go to 10
     close (unit=9)
-
-    ! write out the array of data
-    if (write_data) then
-        open (unit=10,recl=30000,file='data_array.csv',form='formatted')
-        write (10,'(2000(i4,'',''))') ((row_number(ir),row_number(ir),row_number(ir)),ir=1,ntest)
-        write (10,'(2000(a,'',''))') ((trim(adjustL(test_column_names(ir,ic))),ic=1,3),ir=1,ntest)
-        all_data_row_values = ' '
-        do ir = 1, test_row_max
-            do it = 1, ntest
-                ic = 3*it - 2
-                if (ir<=test_length(it)) then
-                    write(all_data_row_values(ic),'(e15.6)') all_data(it,ir,1)
-                    write(all_data_row_values(ic+1),'(e15.6)') all_data(it,ir,2)
-                    write(all_data_row_values(ic+2),'(e15.6)') all_data(it,ir,3)
-                else
-                    all_data_row_values(ic) = ' '
-                    all_data_row_values(ic+1) = ' '
-                    all_data_row_values(ic+2) = ' '
-                endif
-            end do
-        write (10,'(2000(a,'',''))') ((trim(adjustL(all_data_row_values((it-1)*3+ic))),ic=1,3),it=1,ntest)
-    end do
-        close (unit=10)
-    end if
 
     end program VandV_Calcs
 

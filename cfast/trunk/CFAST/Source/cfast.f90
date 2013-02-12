4ec96e2f8738b5c8c704eb1674704b739a1e2844
cfastdev <cfastdev@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2013-02-12 20:12:39 +0000

    CFAST source: updated creation date of software just to trigger a cfastbot run

--- cfast/trunk/CFAST/Source/cfast.f90
+++ cfast/trunk/CFAST/Source/cfast.f90
@@ -38,7 +38,7 @@
     real*8 :: xdelt, tstop, tbeg, tend
 
     version = 6300          ! Current CFAST version number
-    crdate = (/2013,1,15/)  ! Current CFAST executable creation date
+    crdate = (/2013,2,12/)  ! Current CFAST executable creation date
 
     errorcode = 0
 

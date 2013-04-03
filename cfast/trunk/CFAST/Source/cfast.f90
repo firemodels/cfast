997667b5c37af73d977cac626779c02d9b72324d
cfastdev <cfastdev@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2013-04-03 16:01:16 +0000

    CFAST source: dummy edit to trigger cfastbot

--- cfast/trunk/CFAST/Source/cfast.f90
+++ cfast/trunk/CFAST/Source/cfast.f90
@@ -25,8 +25,6 @@
     !     whether in an action of contract, tort or otherwise, arising
     !     from, out of or in connection with the software or the use or
     !     other dealings in the software.
-    
-    ! dummy edit to trigger cfastbot
 
     use cfast_main
     use cparams

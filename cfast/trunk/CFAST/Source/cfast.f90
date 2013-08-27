f2b7e3c416275030bff719a3648f87053a4523c0
cfastdev <cfastdev@gmail.com>
2013-08-27 19:27:36 +0000

    cfastbot: dummy change to force cfastbot

--- cfast/trunk/CFAST/Source/cfast.f90
+++ cfast/trunk/CFAST/Source/cfast.f90
@@ -26,6 +26,7 @@
     !     from, out of or in connection with the software or the use or
     !     other dealings in the software.
     !
+    ! dummy change to force cfastbot
     
     use precision_parameters
     use cfast_main

ec7c523638996ce9c83f9482746d4136277b8ed8
gforney <gforney@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2013-09-11 13:08:12 +0000

    vv: add makefile and build directories/scripts for building VandV_Calcs from the command line

---
+++ cfast/trunk/VandV_Calcs/intel_win_32/make_vv.bat
@@ -0,0 +1,12 @@
+set intelbin="%IFORT_COMPILER13%\bin"
+
+IF "%SETUP_IFORT_COMPILER_32%"=="1" GOTO envexist
+
+set SETUP_IFORT_COMPILER_32=1
+
+echo Setting up compiler environment
+call %intelbin%\ifortvars ia32
+
+:envexist
+make VPATH=".." -f ..\makefile intel_win_32
+pause

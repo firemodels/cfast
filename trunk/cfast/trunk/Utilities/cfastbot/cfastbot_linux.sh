7ae52459bac31ab88c7547d2ff20e5279f8308cf
gforney <gforney@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2013-09-14 02:30:12 +0000

    vv: remove grep for snsq (is handled by grep for ***Error

--- trunk/cfast/trunk/Utilities/cfastbot/cfastbot_linux.sh
+++ trunk/cfast/trunk/Utilities/cfastbot/cfastbot_linux.sh
@@ -381,7 +381,6 @@ check_vv_cases_debug()
    if [[ `grep 'Run aborted' -rI --include *.log ${CFASTBOT_DIR}/output/stage3` == "" ]] && \
       [[ `grep "***Error" -riI --include *.log *` == "" ]] && \
       [[ `grep "***Fatal error" -riI --include *.log *` == "" ]] && \
-      [[ `grep snsq -rI --include *.log *` == "" ]] && \
       [[ `grep -A 20 forrtl -rI --include *.log *` == "" ]]
    then
       :
@@ -389,7 +388,6 @@ check_vv_cases_debug()
       grep 'Run aborted' -rI --include *.log $CFASTBOT_DIR/output/stage3 >> $CFASTBOT_DIR/output/stage3_errors
       grep "***Error" -riI --include *.log * >> $CFASTBOT_DIR/output/stage3_errors
       grep "***Fatal error" -riI --include *.log * >> $CFASTBOT_DIR/output/stage3_errors
-      grep snsq -rI --include *.log * >> $CFASTBOT_DIR/output/stage3_errors
       grep -A 20 forrtl -rI --include *.log * >> $CFASTBOT_DIR/output/stage3_errors
       
       echo "Errors from Stage 3 - Run V&V cases (debug mode):" >> $ERROR_LOG
@@ -404,7 +402,6 @@ check_vv_cases_debug()
    if [[ `grep 'Run aborted' -rI --include *.log ${CFASTBOT_DIR}/output/stage3` == "" ]] && \
       [[ `grep "***Error" -rI --include *.log *` == "" ]] && \
       [[ `grep "***Fatal error" -rI --include *.log *` == "" ]] && \
-      [[ `grep snsq -rI --include *.log *` == "" ]] && \
       [[ `grep -A 20 forrtl -rI --include *.log *` == "" ]]
    then
       :
@@ -412,7 +409,6 @@ check_vv_cases_debug()
       grep 'Run aborted' -rI --include *.log $CFASTBOT_DIR/output/stage3 >> $CFASTBOT_DIR/output/stage3_errors
       grep "***Error" -riI --include *.log * >> $CFASTBOT_DIR/output/stage3_errors
       grep "***Fatal error" -riI --include *.log * >> $CFASTBOT_DIR/output/stage3_errors
-      grep snsq -rI --include *.log * >> $CFASTBOT_DIR/output/stage3_errors
       grep -A 20 forrtl -rI --include *.log * >> $CFASTBOT_DIR/output/stage3_errors
       
       echo "Errors from Stage 3 - Run V&V cases (debug mode):" >> $ERROR_LOG

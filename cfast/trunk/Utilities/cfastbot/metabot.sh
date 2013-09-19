310e7ecbdabcfe854a0faef235a4f0574d001c35
gforney <gforney@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2013-09-19 18:50:23 +0000

    vv: update metabot

--- cfast/trunk/Utilities/cfastbot/metabot.sh
+++ cfast/trunk/Utilities/cfastbot/metabot.sh
@@ -5,14 +5,15 @@ CFASTBOT_DIR="$CFASTBOT_HOME_DIR/CFASTBOT"
 CFAST_SVNROOT="$CFASTBOT_HOME_DIR/cfast"
 FINISH=$CFASTBOT_DIR/output/finished
 
-cd $CFASTBOT_DIR
-for f in r30 r35 r40 r45 r50 r55 r60 r65 r70 
+#for f in r30 r35 r40 r45 r50 r55 r60 r65 r70 
+for f in r30 r35 
    while [ ! -f $FINISH ]; do
       echo file $FINISH does not exist
       sleep 30
    done
+   cp ~/radtest/$f $CFAST_SVNROOT/CFAST/Source/radiation.f90
+
    cd $CFASTBOT_DIR
-   cp $f $CFAST_SVNROOT/CFAST/Source/radiation.f90
    ./run_cfastbot.sh -s -l $f
    sleep 30
 done

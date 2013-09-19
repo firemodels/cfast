9545ee1e94bebc16ebd30594d455450d7bbbf69a
gforney <gforney@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2013-09-19 19:18:27 +0000

    vv: update metabot

--- cfast/trunk/Utilities/cfastbot/metabot.sh
+++ cfast/trunk/Utilities/cfastbot/metabot.sh
@@ -3,12 +3,12 @@
 CFASTBOT_HOME_DIR=~
 CFASTBOT_DIR="$CFASTBOT_HOME_DIR/CFASTBOT"
 CFAST_SVNROOT="$CFASTBOT_HOME_DIR/cfast"
-STARTED=$CFASTBOT_DIR/output/started
+RUNNING=$CFASTBOT_DIR/output/running
 
 #for f in r30 r35 r40 r45 r50 r55 r60 r65 r70 
 for f in r30 r35 
 do
-   while [ -e $STARTED ]; do
+   while [ -e $RUNNING ]; do
       echo trying to run case $f
       echo cfastbot already running
       sleep 30

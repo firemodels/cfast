4055bea543be3c61bd84db1b52cbbfd9fd289d80
koverholt@gmail.com <koverholt@gmail.com@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2013-09-26 10:07:15 +0000

    CFASTbot: Minor formatting edit

--- trunk/cfast/trunk/Utilities/cfastbot/cfastbot_linux.sh
+++ trunk/cfast/trunk/Utilities/cfastbot/cfastbot_linux.sh
@@ -792,7 +792,7 @@ check_validation_stats()
 
    if [ -e ${CURRENT_STATS_FILE} ]
    then
-      if [[ `diff -u -U100 <(sed 's/"//g' ${BASELINE_STATS_FILE}) <(sed 's/"//g' ${CURRENT_STATS_FILE})` == "" ]]
+      if [[ `diff -u <(sed 's/"//g' ${BASELINE_STATS_FILE}) <(sed 's/"//g' ${CURRENT_STATS_FILE})` == "" ]]
       then
          # Continue along
          :
@@ -801,9 +801,9 @@ check_validation_stats()
          echo "Validation statistics have changed since last run." >> $WARNING_LOG
          echo "-------------------------------" >> $WARNING_LOG
          echo "Difference between validation statistics files," >> $WARNING_LOG
-         echo "Baseline validation statistics vs. (Revision ${SVN_REVISION}):" >> $WARNING_LOG
+         echo "Baseline validation statistics vs. Revision ${SVN_REVISION}:" >> $WARNING_LOG
          echo "-------------------------------" >> $WARNING_LOG
-         diff -u -U100 <(sed 's/"//g' ${BASELINE_STATS_FILE}) <(sed 's/"//g' ${CURRENT_STATS_FILE}) >> $WARNING_LOG
+         diff -u <(sed 's/"//g' ${BASELINE_STATS_FILE}) <(sed 's/"//g' ${CURRENT_STATS_FILE}) >> $WARNING_LOG
          echo "" >> $WARNING_LOG
       fi
    else

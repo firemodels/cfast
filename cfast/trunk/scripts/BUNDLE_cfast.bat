65fdec20fc729e4d64e8d7b28445d018a31ebe11
gforney <gforney@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2014-08-12 15:01:20 +0000

    bundle script: consolidate scripts, put stuff from cfast_env into BUNDLE_cfast, remove cfast_env

--- cfast/trunk/scripts/BUNDLE_cfast.bat
+++ cfast/trunk/scripts/BUNDLE_cfast.bat
@@ -1,31 +1,33 @@
 :: @echo off
 Title Bundle cfast for Windows
 
-set basename=cftest6p1
 
-:: Script to bundle cfast into an installation file
+:: installation settings settings
 
-set envfile=%userprofile%\cfast_env.bat
-IF EXIST %envfile% GOTO endif_envexist
-echo ***Fatal error.  The environment setup file %envfile% does not exist. 
-echo Aborting now...
-pause>NUL
-goto:eof
+set installerbase=cftest6
+set cfast_version=6.1.11
+set distname=cfast%cfast_version%
 
-:endif_envexist
+:: VVVVVVVVVVVVVVVVV shouldn't need to change anything below VVVVVVVVVVVVVVV
 
-call %envfile%
-%svn_drive%
 
 set CURDIR=%CD%
-set DISTDIR=%svn_root%\scripts\BUNDLEDIR\%basename%
+cd ..
+set svn_root=%CD%
+cd %CURDIR%
+set svn_drive=c:
+
+%svn_drive%
+
+set DISTDIR=%svn_root%\scripts\BUNDLEDIR\%installerbase%
 set bundleinfo=%svn_root%\scripts\bundleinfo
 
 copy "%bundleinfo%\wrapup_cfast_install.bat" "%DISTDIR%\wrapup_cfast_install.bat"
+:: erase or comment out following line once actual wrapup batch file is complete
 copy "%bundleinfo%\wrapup_dummy.bat" "%DISTDIR%\wrapup_cfast_install.bat"
 
 cd %DISTDIR%
-wzzip -a -r -P ..\%basename%.zip * > Nul
+wzzip -a -r -P ..\%installerbase%.zip * > Nul
 
 :: create an installation file from the zipped bundle directory
 
@@ -36,8 +38,8 @@ echo.
 cd %DISTDIR%\..
 echo Setup is about to install CFAST %cfast_version% > %bundleinfo%\message.txt
 echo Press Setup to begin installation. > %bundleinfo%\main.txt
-if exist %basename%.exe erase %basename%.exe
-wzipse32 %basename%.zip -runasadmin -a %bundleinfo%\about.txt -st"cfast %cfast_version% Setup" -d "c:\Program Files\cfast%cfast_version%" -c wrapup_cfast_install.bat
+if exist %installerbase%.exe erase %installerbase%.exe
+wzipse32 %installerbase%.zip -runasadmin -a %bundleinfo%\about.txt -st"cfast %cfast_version% Setup" -d "c:\Program Files\%distname%" -c wrapup_cfast_install.bat
 
 
 echo.

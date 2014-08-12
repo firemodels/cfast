0dd6af5724ffa8a7fd19ba3b41f78fdee49f9ada
gforney <gforney@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2014-08-12 03:33:38 +0000

    cfast build: define compiler environment when building cfast on the mac

--- cfast/trunk/CFAST/intel_osx_64/make_cfast.sh
+++ cfast/trunk/CFAST/intel_osx_64/make_cfast.sh
@@ -3,5 +3,7 @@ platform=intel64
 dir=`pwd`
 target=${dir##*/}
 
+source $IFORT_COMPILER/bin/compilervars.sh $platform
+
 echo Building $target
 make VPATH="../Source:../Include" -f ../makefile $target

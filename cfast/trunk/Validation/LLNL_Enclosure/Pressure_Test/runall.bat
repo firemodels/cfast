65d7d5772b5054702aceff7fa5bca76c7328a1e1
cfastdev <cfastdev@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2014-11-06 18:19:19 +0000

    Validation: Update LLNL Ambient Pressure test batch file to current file structure

--- cfast/trunk/Validation/LLNL_Enclosure/Pressure_Test/runall.bat
+++ cfast/trunk/Validation/LLNL_Enclosure/Pressure_Test/runall.bat
@@ -1,68 +1,69 @@
 echo off
 call ..\..\cleancfast.bat
-..\..\..\bin\cfast LLNL_01p /V
-..\..\..\bin\cfast LLNL_02p /V
-..\..\..\bin\cfast LLNL_03p /V
-..\..\..\bin\cfast LLNL_04p /V
-..\..\..\bin\cfast LLNL_05p /V
-..\..\..\bin\cfast LLNL_06p /V
-..\..\..\bin\cfast LLNL_07p /V
-..\..\..\bin\cfast LLNL_08p /V
-..\..\..\bin\cfast LLNL_09p /V
-..\..\..\bin\cfast LLNL_10p /V
-..\..\..\bin\cfast LLNL_11p /V
-..\..\..\bin\cfast LLNL_12p /V
-..\..\..\bin\cfast LLNL_13p /V
-..\..\..\bin\cfast LLNL_14p /V
-..\..\..\bin\cfast LLNL_15p /V
-..\..\..\bin\cfast LLNL_16p /V
-..\..\..\bin\cfast LLNL_17p /V
-..\..\..\bin\cfast LLNL_18p /V
-..\..\..\bin\cfast LLNL_19p /V
-..\..\..\bin\cfast LLNL_20p /V
-..\..\..\bin\cfast LLNL_21p /V
-..\..\..\bin\cfast LLNL_22p /V
-..\..\..\bin\cfast LLNL_23p /V
-..\..\..\bin\cfast LLNL_24p /V
-..\..\..\bin\cfast LLNL_25p /V
-..\..\..\bin\cfast LLNL_26p /V
-..\..\..\bin\cfast LLNL_27p /V
-..\..\..\bin\cfast LLNL_28p /V
-..\..\..\bin\cfast LLNL_29p /V
-..\..\..\bin\cfast LLNL_30p /V
-..\..\..\bin\cfast LLNL_31p /V
-..\..\..\bin\cfast LLNL_32p /V
-..\..\..\bin\cfast LLNL_33p /V
-..\..\..\bin\cfast LLNL_34p /V
-..\..\..\bin\cfast LLNL_35p /V
-..\..\..\bin\cfast LLNL_36p /V
-..\..\..\bin\cfast LLNL_37p /V
-..\..\..\bin\cfast LLNL_38p /V
-..\..\..\bin\cfast LLNL_39p /V
-..\..\..\bin\cfast LLNL_40p /V
-..\..\..\bin\cfast LLNL_41p /V
-..\..\..\bin\cfast LLNL_42p /V
-..\..\..\bin\cfast LLNL_43p /V
-..\..\..\bin\cfast LLNL_44p /V
-..\..\..\bin\cfast LLNL_45p /V
-..\..\..\bin\cfast LLNL_46p /V
-..\..\..\bin\cfast LLNL_47p /V
-..\..\..\bin\cfast LLNL_48p /V
-..\..\..\bin\cfast LLNL_49p /V
-..\..\..\bin\cfast LLNL_50p /V
-..\..\..\bin\cfast LLNL_51p /V
-..\..\..\bin\cfast LLNL_52p /V
-..\..\..\bin\cfast LLNL_53p /V
-..\..\..\bin\cfast LLNL_54p /V
-..\..\..\bin\cfast LLNL_55p /V
-..\..\..\bin\cfast LLNL_56p /V
-..\..\..\bin\cfast LLNL_57p /V
-..\..\..\bin\cfast LLNL_58p /V
-..\..\..\bin\cfast LLNL_59p /V
-..\..\..\bin\cfast LLNL_60p /V
-..\..\..\bin\cfast LLNL_61p /V
-..\..\..\bin\cfast LLNL_62p /V
-..\..\..\bin\cfast LLNL_63p /V
-..\..\..\bin\cfast LLNL_64p /V
-..\..\..\VandV_Calcs\Release\VandV_Calcs.exe CFAST_Pressure_Correction_Inputs.csv
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_01p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_02p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_03p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_04p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_05p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_06p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_07p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_08p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_09p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_10p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_11p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_12p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_13p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_14p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_15p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_16p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_17p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_18p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_19p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_20p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_21p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_22p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_23p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_24p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_25p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_26p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_27p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_28p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_29p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_30p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_31p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_32p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_33p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_34p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_35p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_36p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_37p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_38p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_39p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_40p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_41p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_42p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_43p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_44p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_45p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_46p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_47p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_48p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_49p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_50p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_51p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_52p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_53p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_54p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_55p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_56p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_57p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_58p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_59p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_60p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_61p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_62p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_63p /V
+..\..\scripts\background -u 98  ..\..\..\bin\cfast LLNL_64p /V
+cd ..\..
+..\VandV_Calcs\Release\VandV_Calcs.exe CFAST_Pressure_Correction_Inputs.csv
 copy ..\..\pressures.csv LLNL_Ambient_pressures.csv /Y

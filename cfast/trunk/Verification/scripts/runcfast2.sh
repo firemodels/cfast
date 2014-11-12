323ee9f20cfe9107de7e19d1e0eaa06d204c284e
cfastdev <cfastdev@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2014-11-12 20:44:33 +0000

    Scripts: Add script to run CFAST two folders down to verification scripts.  This is just copied from the validation scripts

---
+++ cfast/trunk/Verification/scripts/runcfast2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -f
+dir=$1
+dir2=$2
+infile=$3
+
+fulldir=$BASEDIR/$dir/$dir2
+
+if ! [ -e $CFAST ];  then
+  echo "The file $CFAST does not exit. Run aborted"
+  exit
+fi
+if ! [ -d $fulldir ]; then
+  echo "The directory $fulldir does not exit. Run aborted."
+  exit
+fi
+if ! [ -e $fulldir/$infile.in ]; then
+  echo "The input file, $fulldir/$infile.in, does not exit. Run aborted."
+  exit
+fi
+cd $fulldir
+echo -----------------------------
+echo running $infile in $fulldir
+$CFAST $infile /V
+echo -----------------------------

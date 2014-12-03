534a7cc77c8d4fa428a7d9c50622216568bd0186
gforney <gforney@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2014-12-03 13:40:32 +0000

    scripts: add batch file for building the tech guide on windows

---
+++ cfast/trunk/Docs/Tech_Ref/make_guide.bat
@@ -0,0 +1,19 @@
+@echo off
+set paper=Tech_Ref
+
+echo pass 1
+pdflatex -interaction nonstopmode %paper% > %paper%.err
+bibtex %paper% > %paper%.err
+echo pass 2
+pdflatex -interaction nonstopmode %paper% > %paper%.err
+echo pass 3
+pdflatex -interaction nonstopmode %paper% > %paper%.err
+
+find "! LaTeX Error:" %paper%.err
+find "Fatal error" %paper%.err
+find "Error:" %paper%.err
+
+echo %paper% build complete
+pause
+
+

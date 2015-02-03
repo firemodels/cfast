f5a196b6788961753557f48f65c268441707429a
gforney <gforney@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2015-02-03 20:41:17 +0000

    cfast source: add version of SPLITPATHQQ for gnu builds

--- cfast/trunk/CFAST/Source/keyboard_gnu.f90
+++ cfast/trunk/CFAST/Source/keyboard_gnu.f90
@@ -13,3 +13,55 @@
     return
     end subroutine grabky
 
+! ---------------------------  -------------------------------------------
+
+    integer function splitpathqq(path,drive,dir,name,ext)
+    character*(*), intent(IN) :: path
+    character*(*), intent(out) :: drive, dir, name, ext
+   
+    character*1 dirsep
+    integer :: i, len, first, next
+
+    dirsep = '/'
+    drive=""
+    dir=""
+    name=""
+    ext=""
+    len = length(trim(path))
+    splitpathqq=len
+    if(len.eq.0)return
+    ! look for first non-blank character
+    do i = 1, len
+       first=i
+       if(path(i:i).ne.' ')exit
+    end do
+
+    ! look for extension
+    next=len
+    do i = next, first, -1
+       if(path(i:i)=='.')then
+          ext=path(i:len)
+          next=i-1
+          exit
+       endif
+    end do
+
+    ! look for filename
+    do i = next, first, -1
+       if(path(i:i)==dirsep)then
+          name=path(i+1:next)
+          next=i
+          exit
+       endif
+    end do
+    ! directory
+    dir=path(first:next)
+
+! debug print
+!    write(6,*)"path=",trim(path)
+!    write(6,*)"drive=",trim(drive)
+!    write(6,*)"dir=",trim(dir)
+!    write(6,*)"name=",trim(name)
+!    write(6,*)"ext=",trim(ext)
+!    stop    
+    end function splitpathqq

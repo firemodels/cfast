0f037c2601183f6657cc34b9b5d5f57395340399
gforney <gforney@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2015-02-03 19:16:56 +0000

    cfast source: move grabkey into a separate file (one file for gnu, one file for intel)

--- cfast/trunk/CFAST/Source/auxiliary.f90
+++ cfast/trunk/CFAST/Source/auxiliary.f90
@@ -791,35 +791,6 @@
 
     end subroutine exehandle
 
-! --------------------------- grabky -------------------------------------------
-
-    subroutine grabky (ich,it)
-
-    implicit none
-    
-    integer(2), intent(out) :: ich, it
-
-    character(1) :: ch, getcharqq
-    logical :: peekcharqq
-
-    ich = 0
-    it = 0
-
-    if (peekcharqq()) then
-        ch = getcharqq()
-        ich = ichar(ch)
-        if (ich==0) then
-            ch = getcharqq()
-            ich = ichar (ch)
-            it = 2
-        else
-            it = 1
-        endif
-    endif
-
-    return
-    end subroutine grabky
-
 ! --------------------------- mat2mult -------------------------------------------
 
     subroutine mat2mult(mat1,mat2,idim,n)

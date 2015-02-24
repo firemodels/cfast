8d92d63a86aaa4de2e6611e0c58fca99f7d0e6e6
cfastdev <cfastdev@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2015-02-24 18:13:05 +0000

    CFAST V&V: Update scripts to eliminate useless error messages

--- trunk/cfast/trunk/Validation/Cleanall.bat
+++ trunk/cfast/trunk/Validation/Cleanall.bat
@@ -1,28 +1,28 @@
 echo off
 echo Removing old graphics files.  Messages can be ignored here.
-del ..\Docs\Validation_Guide\FIGURES\ATF_Corridors\ATF_Corridors_HGL*kW.pdf /q >nul
-del ..\Docs\Validation_Guide\FIGURES\ATF_Corridors\ATF_Corridors_Jet*kW.pdf /q >nul
-del ..\Docs\Validation_Guide\FIGURES\FM_NBS\fm??_*.pdf /q >nul
-del ..\Docs\Validation_Guide\FIGURES\FM_SNL\FM_SNL_*.pdf /q >nul
-del ..\Docs\Validation_Guide\FIGURES\High_Bay\USN_Hawaii_Test_??*.pdf /q >nul
-del ..\Docs\Validation_Guide\FIGURES\High_Bay\USN_Iceland_Test_??*.pdf /q >nul
-del ..\Docs\Validation_Guide\FIGURES\iBMB\iBMB_Cable_*.pdf /q >nul
-del ..\Docs\Validation_Guide\FIGURES\iBMB\iBMB_Pool_*.pdf /q >nul
-del ..\Docs\Validation_Guide\FIGURES\LLNL_Enclosure\LLNL_??_*.pdf /q >nul
-del ..\Docs\Validation_Guide\FIGURES\NBS\1r*.pdf /q >nul
-del ..\Docs\Validation_Guide\FIGURES\NBS\NBS_100*.pdf /q >nul
-del ..\Docs\Validation_Guide\FIGURES\NIST_Dunes_2000\NIST_Dunes_2000_SD*.pdf
-del ..\Docs\Validation_Guide\FIGURES\NIST_NRC\NIST_NRC_??_*.pdf /q >nul
-del ..\Docs\Validation_Guide\FIGURES\NIST_PLAZA\Room*.pdf /q >nul
-del ..\Docs\Validation_Guide\FIGURES\SP_AST\SP_AST_Diesel*.pdf /q >nul
-del ..\Docs\Validation_Guide\FIGURES\SP_AST\SP_AST_Heptane*.pdf /q >nul
-del ..\Docs\Validation_Guide\FIGURES\SP_AST\SP_AST_Test_*.pdf /q >nul
-del ..\Docs\Validation_Guide\FIGURES\Steckler_Compartment\Steckler_???*.pdf /q >nul
-del ..\Docs\Validation_Guide\FIGURES\UL_NFPRF\UL_NFPRF_?_??_j*.pdf /q >nul
-del ..\Docs\Validation_Guide\FIGURES\UL_NFPRF\UL_NFPRF_?_??_Sp*.pdf /q >nul
-del ..\Docs\Validation_Guide\FIGURES\UL_NIST_Vents\UL_NIST_Vents_Test*.pdf /q >nul
-del ..\Docs\Validation_Guide\FIGURES\Vettori_Flat\Activation_Test_??.pdf /q >nul
-del ..\Docs\Validation_Guide\FIGURES\Vettori_Flat\Test_??.pdf /q >nul
-del ..\Docs\Validation_Guide\FIGURES\VTT\VTT_??_*.pdf /q >nul
-del ..\Docs\Validation_Guide\FIGURES\WTC\WTC_??_*.pdf /q >nul
+del ..\Docs\Validation_Guide\FIGURES\ATF_Corridors\ATF_Corridors_HGL*kW.pdf /q >nul 2>&1
+del ..\Docs\Validation_Guide\FIGURES\ATF_Corridors\ATF_Corridors_Jet*kW.pdf /q >nul 2>&1
+del ..\Docs\Validation_Guide\FIGURES\FM_NBS\fm??_*.pdf /q >nul 2>&1
+del ..\Docs\Validation_Guide\FIGURES\FM_SNL\FM_SNL_*.pdf /q >nul 2>&1
+del ..\Docs\Validation_Guide\FIGURES\High_Bay\USN_Hawaii_Test_??*.pdf /q >nul 2>&1
+del ..\Docs\Validation_Guide\FIGURES\High_Bay\USN_Iceland_Test_??*.pdf /q >nul 2>&1
+del ..\Docs\Validation_Guide\FIGURES\iBMB\iBMB_Cable_*.pdf /q >nul 2>&1
+del ..\Docs\Validation_Guide\FIGURES\iBMB\iBMB_Pool_*.pdf /q >nul 2>&1
+del ..\Docs\Validation_Guide\FIGURES\LLNL_Enclosure\LLNL_??_*.pdf /q >nul 2>&1
+del ..\Docs\Validation_Guide\FIGURES\NBS\1r*.pdf /q >nul 2>&1
+del ..\Docs\Validation_Guide\FIGURES\NBS\NBS_100*.pdf /q >nul 2>&1
+del ..\Docs\Validation_Guide\FIGURES\NIST_Dunes_2000\NIST_Dunes_2000_SD*.pdf 2>&1
+del ..\Docs\Validation_Guide\FIGURES\NIST_NRC\NIST_NRC_??_*.pdf /q >nul 2>&1
+del ..\Docs\Validation_Guide\FIGURES\NIST_PLAZA\Room*.pdf /q >nul 2>&1
+del ..\Docs\Validation_Guide\FIGURES\SP_AST\SP_AST_Diesel*.pdf /q >nul 2>&1
+del ..\Docs\Validation_Guide\FIGURES\SP_AST\SP_AST_Heptane*.pdf /q >nul 2>&1
+del ..\Docs\Validation_Guide\FIGURES\SP_AST\SP_AST_Test_*.pdf /q >nul 2>&1
+del ..\Docs\Validation_Guide\FIGURES\Steckler_Compartment\Steckler_???*.pdf /q >nul 2>&1
+del ..\Docs\Validation_Guide\FIGURES\UL_NFPRF\UL_NFPRF_?_??_j*.pdf /q >nul 2>&1
+del ..\Docs\Validation_Guide\FIGURES\UL_NFPRF\UL_NFPRF_?_??_Sp*.pdf /q >nul 2>&1
+del ..\Docs\Validation_Guide\FIGURES\UL_NIST_Vents\UL_NIST_Vents_Test*.pdf /q >nul 2>&1
+del ..\Docs\Validation_Guide\FIGURES\Vettori_Flat\Activation_Test_??.pdf /q >nul 2>&1
+del ..\Docs\Validation_Guide\FIGURES\Vettori_Flat\Test_??.pdf /q >nul 2>&1
+del ..\Docs\Validation_Guide\FIGURES\VTT\VTT_??_*.pdf /q >nul 2>&1
+del ..\Docs\Validation_Guide\FIGURES\WTC\WTC_??_*.pdf /q >nul 2>&1
 echo Done removing old and temporary files.

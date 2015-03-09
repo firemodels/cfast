3f77b7d7c47a6a295236dd62c2460a0791d66411
cfastdev <cfastdev@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2015-03-09 20:32:45 +0000

    CEdit Source: Change animated image to a new Smokeview animation

--- trunk/cfast/trunk/Cedit/About.vb
+++ trunk/cfast/trunk/Cedit/About.vb
@@ -115,6 +115,7 @@ Public Class About
         Me.PictureBox1.Location = New System.Drawing.Point(376, 16)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(136, 96)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.PictureBox1.TabIndex = 6
         Me.PictureBox1.TabStop = False
         '

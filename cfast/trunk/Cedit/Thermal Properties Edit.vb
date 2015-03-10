8115acb333399eb05367479a5771d381b103944b
cfastdev <cfastdev@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2015-03-10 20:02:41 +0000

    CEdit Source: Minor visual updates to CEdit for version 7 documentation

--- cfast/trunk/Cedit/Thermal Properties Edit.vb
+++ cfast/trunk/Cedit/Thermal Properties Edit.vb
@@ -48,20 +48,6 @@ Public Class Thermal_Properties_Edit
     Friend WithEvents ThermalThickness As System.Windows.Forms.TextBox
     Friend WithEvents ThermalEmissivity As System.Windows.Forms.TextBox
     Friend WithEvents ThermalShortName As System.Windows.Forms.TextBox
-    Friend WithEvents ThermalHclb1 As System.Windows.Forms.TextBox
-    Friend WithEvents Label8 As System.Windows.Forms.Label
-    Friend WithEvents ThermalHclb2 As System.Windows.Forms.TextBox
-    Friend WithEvents Label9 As System.Windows.Forms.Label
-    Friend WithEvents ThermalHclb3 As System.Windows.Forms.TextBox
-    Friend WithEvents Label10 As System.Windows.Forms.Label
-    Friend WithEvents ThermalHclb6 As System.Windows.Forms.TextBox
-    Friend WithEvents Label11 As System.Windows.Forms.Label
-    Friend WithEvents ThermalHclb5 As System.Windows.Forms.TextBox
-    Friend WithEvents Label12 As System.Windows.Forms.Label
-    Friend WithEvents ThermalHclb4 As System.Windows.Forms.TextBox
-    Friend WithEvents Label13 As System.Windows.Forms.Label
-    Friend WithEvents ThermalHclb7 As System.Windows.Forms.TextBox
-    Friend WithEvents Label14 As System.Windows.Forms.Label
     Friend WithEvents ThermalSummary As C1.Win.C1FlexGrid.C1FlexGrid
     Friend WithEvents ThermalConductivity As System.Windows.Forms.TextBox
     Friend WithEvents GroupThermal As System.Windows.Forms.GroupBox
@@ -88,20 +74,6 @@ Public Class Thermal_Properties_Edit
         Me.Label6 = New System.Windows.Forms.Label()
         Me.ThermalShortName = New System.Windows.Forms.TextBox()
         Me.Label7 = New System.Windows.Forms.Label()
-        Me.ThermalHclb1 = New System.Windows.Forms.TextBox()
-        Me.Label8 = New System.Windows.Forms.Label()
-        Me.ThermalHclb2 = New System.Windows.Forms.TextBox()
-        Me.Label9 = New System.Windows.Forms.Label()
-        Me.ThermalHclb3 = New System.Windows.Forms.TextBox()
-        Me.Label10 = New System.Windows.Forms.Label()
-        Me.ThermalHclb6 = New System.Windows.Forms.TextBox()
-        Me.Label11 = New System.Windows.Forms.Label()
-        Me.ThermalHclb5 = New System.Windows.Forms.TextBox()
-        Me.Label12 = New System.Windows.Forms.Label()
-        Me.ThermalHclb4 = New System.Windows.Forms.TextBox()
-        Me.Label13 = New System.Windows.Forms.Label()
-        Me.ThermalHclb7 = New System.Windows.Forms.TextBox()
-        Me.Label14 = New System.Windows.Forms.Label()
         Me.GroupThermal = New System.Windows.Forms.GroupBox()
         Me.ThermalRemove = New System.Windows.Forms.Button()
         Me.ThermalAdd = New System.Windows.Forms.Button()
@@ -136,7 +108,7 @@ Public Class Thermal_Properties_Edit
         'ThermalOK
         '
         Me.ThermalOK.DialogResult = System.Windows.Forms.DialogResult.OK
-        Me.ThermalOK.Location = New System.Drawing.Point(279, 465)
+        Me.ThermalOK.Location = New System.Drawing.Point(279, 385)
         Me.ThermalOK.Name = "ThermalOK"
         Me.ThermalOK.Size = New System.Drawing.Size(75, 23)
         Me.ThermalOK.TabIndex = 16
@@ -145,7 +117,7 @@ Public Class Thermal_Properties_Edit
         'ThermalCancel
         '
         Me.ThermalCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel
-        Me.ThermalCancel.Location = New System.Drawing.Point(439, 465)
+        Me.ThermalCancel.Location = New System.Drawing.Point(439, 385)
         Me.ThermalCancel.Name = "ThermalCancel"
         Me.ThermalCancel.Size = New System.Drawing.Size(75, 23)
         Me.ThermalCancel.TabIndex = 17
@@ -260,158 +232,25 @@ Public Class Thermal_Properties_Edit
         Me.Label7.Text = "Short Name:"
         Me.Label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
-        'ThermalHclb1
-        '
-        Me.ThermalHclb1.Location = New System.Drawing.Point(128, 111)
-        Me.ThermalHclb1.Name = "ThermalHclb1"
-        Me.ThermalHclb1.Size = New System.Drawing.Size(80, 20)
-        Me.ThermalHclb1.TabIndex = 9
-        '
-        'Label8
-        '
-        Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(80, 111)
-        Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(48, 13)
-        Me.Label8.TabIndex = 17
-        Me.Label8.Text = "HCl (b1):"
-        Me.Label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'ThermalHclb2
-        '
-        Me.ThermalHclb2.Location = New System.Drawing.Point(392, 111)
-        Me.ThermalHclb2.Name = "ThermalHclb2"
-        Me.ThermalHclb2.Size = New System.Drawing.Size(80, 20)
-        Me.ThermalHclb2.TabIndex = 10
-        '
-        'Label9
-        '
-        Me.Label9.AutoSize = True
-        Me.Label9.Location = New System.Drawing.Point(344, 111)
-        Me.Label9.Name = "Label9"
-        Me.Label9.Size = New System.Drawing.Size(48, 13)
-        Me.Label9.TabIndex = 19
-        Me.Label9.Text = "HCl (b2):"
-        Me.Label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'ThermalHclb3
-        '
-        Me.ThermalHclb3.Location = New System.Drawing.Point(616, 111)
-        Me.ThermalHclb3.Name = "ThermalHclb3"
-        Me.ThermalHclb3.Size = New System.Drawing.Size(80, 20)
-        Me.ThermalHclb3.TabIndex = 11
-        '
-        'Label10
-        '
-        Me.Label10.AutoSize = True
-        Me.Label10.Location = New System.Drawing.Point(568, 111)
-        Me.Label10.Name = "Label10"
-        Me.Label10.Size = New System.Drawing.Size(48, 13)
-        Me.Label10.TabIndex = 21
-        Me.Label10.Text = "HCl (b3):"
-        Me.Label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'ThermalHclb6
-        '
-        Me.ThermalHclb6.Location = New System.Drawing.Point(616, 143)
-        Me.ThermalHclb6.Name = "ThermalHclb6"
-        Me.ThermalHclb6.Size = New System.Drawing.Size(80, 20)
-        Me.ThermalHclb6.TabIndex = 14
-        '
-        'Label11
-        '
-        Me.Label11.AutoSize = True
-        Me.Label11.Location = New System.Drawing.Point(568, 143)
-        Me.Label11.Name = "Label11"
-        Me.Label11.Size = New System.Drawing.Size(48, 13)
-        Me.Label11.TabIndex = 27
-        Me.Label11.Text = "HCl (b6):"
-        Me.Label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'ThermalHclb5
-        '
-        Me.ThermalHclb5.Location = New System.Drawing.Point(392, 143)
-        Me.ThermalHclb5.Name = "ThermalHclb5"
-        Me.ThermalHclb5.Size = New System.Drawing.Size(80, 20)
-        Me.ThermalHclb5.TabIndex = 13
-        '
-        'Label12
-        '
-        Me.Label12.AutoSize = True
-        Me.Label12.Location = New System.Drawing.Point(344, 143)
-        Me.Label12.Name = "Label12"
-        Me.Label12.Size = New System.Drawing.Size(48, 13)
-        Me.Label12.TabIndex = 25
-        Me.Label12.Text = "HCl (b5):"
-        Me.Label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'ThermalHclb4
-        '
-        Me.ThermalHclb4.Location = New System.Drawing.Point(128, 143)
-        Me.ThermalHclb4.Name = "ThermalHclb4"
-        Me.ThermalHclb4.Size = New System.Drawing.Size(80, 20)
-        Me.ThermalHclb4.TabIndex = 12
-        '
-        'Label13
-        '
-        Me.Label13.AutoSize = True
-        Me.Label13.Location = New System.Drawing.Point(80, 143)
-        Me.Label13.Name = "Label13"
-        Me.Label13.Size = New System.Drawing.Size(48, 13)
-        Me.Label13.TabIndex = 23
-        Me.Label13.Text = "HCl (b4):"
-        Me.Label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'ThermalHclb7
-        '
-        Me.ThermalHclb7.Location = New System.Drawing.Point(392, 175)
-        Me.ThermalHclb7.Name = "ThermalHclb7"
-        Me.ThermalHclb7.Size = New System.Drawing.Size(80, 20)
-        Me.ThermalHclb7.TabIndex = 15
-        '
-        'Label14
-        '
-        Me.Label14.AutoSize = True
-        Me.Label14.Location = New System.Drawing.Point(344, 175)
-        Me.Label14.Name = "Label14"
-        Me.Label14.Size = New System.Drawing.Size(48, 13)
-        Me.Label14.TabIndex = 29
-        Me.Label14.Text = "HCl (b7):"
-        Me.Label14.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
         'GroupThermal
         '
-        Me.GroupThermal.Controls.Add(Me.ThermalHclb7)
         Me.GroupThermal.Controls.Add(Me.ThermalThickness)
-        Me.GroupThermal.Controls.Add(Me.Label14)
         Me.GroupThermal.Controls.Add(Me.Label1)
-        Me.GroupThermal.Controls.Add(Me.ThermalHclb6)
         Me.GroupThermal.Controls.Add(Me.ThermalConductivity)
-        Me.GroupThermal.Controls.Add(Me.Label11)
         Me.GroupThermal.Controls.Add(Me.Label2)
-        Me.GroupThermal.Controls.Add(Me.ThermalHclb5)
         Me.GroupThermal.Controls.Add(Me.ThermalSpecificHeat)
-        Me.GroupThermal.Controls.Add(Me.Label12)
         Me.GroupThermal.Controls.Add(Me.Label3)
-        Me.GroupThermal.Controls.Add(Me.ThermalHclb4)
         Me.GroupThermal.Controls.Add(Me.ThermalLongName)
-        Me.GroupThermal.Controls.Add(Me.Label13)
         Me.GroupThermal.Controls.Add(Me.Label4)
-        Me.GroupThermal.Controls.Add(Me.ThermalHclb3)
         Me.GroupThermal.Controls.Add(Me.ThermalDensity)
-        Me.GroupThermal.Controls.Add(Me.Label10)
         Me.GroupThermal.Controls.Add(Me.Label5)
-        Me.GroupThermal.Controls.Add(Me.ThermalHclb2)
         Me.GroupThermal.Controls.Add(Me.Label6)
-        Me.GroupThermal.Controls.Add(Me.Label9)
         Me.GroupThermal.Controls.Add(Me.ThermalEmissivity)
-        Me.GroupThermal.Controls.Add(Me.ThermalHclb1)
         Me.GroupThermal.Controls.Add(Me.Label7)
-        Me.GroupThermal.Controls.Add(Me.Label8)
         Me.GroupThermal.Controls.Add(Me.ThermalShortName)
         Me.GroupThermal.Location = New System.Drawing.Point(20, 249)
         Me.GroupThermal.Name = "GroupThermal"
-        Me.GroupThermal.Size = New System.Drawing.Size(752, 210)
+        Me.GroupThermal.Size = New System.Drawing.Size(752, 113)
         Me.GroupThermal.TabIndex = 30
         Me.GroupThermal.TabStop = False
         Me.GroupThermal.Text = "Property 1 (of 1)"
@@ -446,7 +285,7 @@ Public Class Thermal_Properties_Edit
         Me.C1SizerLight1.SetAutoResize(Me, True)
         Me.AutoScaleBaseSize = New System.Drawing.Size(5, 13)
         Me.CancelButton = Me.ThermalCancel
-        Me.ClientSize = New System.Drawing.Size(859, 502)
+        Me.ClientSize = New System.Drawing.Size(793, 423)
         Me.Controls.Add(Me.ThermalRemove)
         Me.Controls.Add(Me.ThermalAdd)
         Me.Controls.Add(Me.ThermalDup)
@@ -455,7 +294,6 @@ Public Class Thermal_Properties_Edit
         Me.Controls.Add(Me.ThermalOK)
         Me.Controls.Add(Me.GroupThermal)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
-        Me.MinimumSize = New System.Drawing.Size(800, 490)
         Me.Name = "Thermal_Properties_Edit"
         Me.ShowInTaskbar = False
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
@@ -470,9 +308,7 @@ Public Class Thermal_Properties_Edit
 
 #End Region
 
-    Private Sub Thermal_Changed(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ThermalLongName.Leave, ThermalShortName.Leave, _
-    ThermalConductivity.Leave, ThermalSpecificHeat.Leave, ThermalDensity.Leave, ThermalThickness.Leave, ThermalEmissivity.Leave, ThermalHclb1.Leave, _
-    ThermalHclb2.Leave, ThermalHclb3.Leave, ThermalHclb4.Leave, ThermalHclb5.Leave, ThermalHclb6.Leave, ThermalHclb7.Leave
+    Private Sub Thermal_Changed(sender As Object, e As EventArgs) Handles ThermalLongName.Leave, ThermalShortName.Leave, ThermalConductivity.Leave, ThermalSpecificHeat.Leave, ThermalDensity.Leave, ThermalThickness.Leave, ThermalEmissivity.Leave
         If CurrentThermalProperty >= 0 And CurrentThermalProperty < TempThermalProperties.Count Then
             Dim aProperty As New ThermalProperty
             aProperty = TempThermalProperties(CurrentThermalProperty)
@@ -483,13 +319,6 @@ Public Class Thermal_Properties_Edit
             If sender Is Me.ThermalDensity Then aProperty.Density = Val(Me.ThermalDensity.Text)
             If sender Is Me.ThermalThickness Then aProperty.Thickness = Val(Me.ThermalThickness.Text)
             If sender Is Me.ThermalEmissivity Then aProperty.Emissivity = Val(Me.ThermalEmissivity.Text)
-            If sender Is Me.ThermalHclb1 Then aProperty.HClDeposition(0) = Val(Me.ThermalHclb1.Text)
-            If sender Is Me.ThermalHclb2 Then aProperty.HClDeposition(1) = Val(Me.ThermalHclb2.Text)
-            If sender Is Me.ThermalHclb3 Then aProperty.HClDeposition(2) = Val(Me.ThermalHclb3.Text)
-            If sender Is Me.ThermalHclb4 Then aProperty.HClDeposition(3) = Val(Me.ThermalHclb4.Text)
-            If sender Is Me.ThermalHclb5 Then aProperty.HClDeposition(4) = Val(Me.ThermalHclb5.Text)
-            If sender Is Me.ThermalHclb6 Then aProperty.HClDeposition(5) = Val(Me.ThermalHclb6.Text)
-            If sender Is Me.ThermalHclb7 Then aProperty.HClDeposition(6) = Val(Me.ThermalHclb7.Text)
             UpdateThermalProperties(CurrentThermalProperty)
         End If
     End Sub
@@ -511,7 +340,6 @@ Public Class Thermal_Properties_Edit
             UpdateThermalProperties(CurrentThermalProperty)
         End If
     End Sub
-
     Private Sub Thermal_Properties_Edit_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         Dim FromMaterial As New ThermalProperty
         Dim i As Integer
@@ -537,15 +365,6 @@ Public Class Thermal_Properties_Edit
             Me.ThermalDensity.Text = aThermalProperty.Density.ToString + myUnits.Convert(UnitsNum.Density).Units
             Me.ThermalThickness.Text = aThermalProperty.Thickness.ToString + myUnits.Convert(UnitsNum.Length).Units
             Me.ThermalEmissivity.Text = aThermalProperty.Emissivity.ToString
-            Dim b(6) As Single
-            aThermalProperty.GetHCl(b)
-            Me.ThermalHclb1.Text = b(0).ToString
-            Me.ThermalHclb2.Text = b(1).ToString
-            Me.ThermalHclb3.Text = b(2).ToString
-            Me.ThermalHclb4.Text = b(3).ToString
-            Me.ThermalHclb5.Text = b(4).ToString
-            Me.ThermalHclb6.Text = b(5).ToString
-            Me.ThermalHclb7.Text = b(6).ToString
             ClearGrid(Me.ThermalSummary)
             For i = 1 To TempThermalProperties.Count
                 aThermalProperty = TempThermalProperties(i - 1)
@@ -621,5 +440,9 @@ Public Class Thermal_Properties_Edit
             End If
             UpdateThermalProperties(CurrentThermalProperty)
         End If
+    End Sub
+
+    Private Sub Thermal_TextChanged(sender As Object, e As EventArgs) Handles ThermalShortName.TextChanged
+
     End Sub
 End Class

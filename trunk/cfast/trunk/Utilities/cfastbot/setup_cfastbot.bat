f01aed6a54089d7872d1bf9a25c68e0c7f1c4892
gforney <gforney@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2015-03-22 05:09:02 +0000

    cfast vv: preliminary update to cfastbot

--- trunk/cfast/trunk/Utilities/cfastbot/setup_cfastbot.bat
+++ trunk/cfast/trunk/Utilities/cfastbot/setup_cfastbot.bat
@@ -24,6 +24,15 @@ if NOT exist %userprofile%\cfastclean (
   svn co http://cfast.googlecode.com/svn/trunk/cfast/trunk cfastclean
 )
 
+:: create a clean FDS repository (used only by cfastbot)
+
+if NOT exist %userprofile%\FDS-SMVclean (
+  cd %userprofile%
+  echo Initializing %userprofile%\FDS-SMVclean.
+  echo Initializing the FDS-SMVclean repository.
+  svn co http://fds-smv.googlecode.com/svn/trunk/FDS/trunk FDS-SMVclean
+)
+
 :: create directory where cfastbot is run
 
 if NOT exist %userprofile%\cfastbot (
@@ -36,19 +45,20 @@ if NOT exist %userprofile%\cfastbot (
 
 call "%svnroot%\scripts\setup_intel_compilers.bat" 1> Nul 2>&1
 call :is_file_installed ifort
-if "%has_ifort% == "0" (
+if "%has_ifort%" == "0" (
   echo Intel Fortran is not installed.
   echo Need to install Intel Fortran before running cfastbot.
 )
 
 :: is Intel C installed
 
-call :is_file_installed icc
-if "%has_icc% == "0" (
+call :is_file_installed icl
+if "%has_icl%" == "0" (
   echo Intel C/C++ is not installed.
   echo Need to install Intel C/C++ before running cfastbot.
 )
 
+echo cfastbot setup complete
 goto :eof
 
 :: -------------------------------------------------------------
@@ -68,5 +78,5 @@ goto :eof
   exit /b 0
 
 :eof
-echo cfastbot setup complete
+
 

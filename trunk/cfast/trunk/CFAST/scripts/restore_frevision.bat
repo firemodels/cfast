1e76cedf0496780ae0ae7d144e4ba72b1ef5d61a
gforney <gforney@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2015-05-13 17:12:20 +0000

    cfast build: corrections to revision generating scripts

--- trunk/cfast/trunk/CFAST/scripts/restore_frevision.bat
+++ trunk/cfast/trunk/CFAST/scripts/restore_frevision.bat
@@ -2,14 +2,14 @@
 set dir=%1
 set file=%2
 
-set CURDIR=%CD%
-
 if NOT exist %dir% (
   exit /b 1
 )
 
 set fullfile=%dir%\%file%
 set fullfilebak=%dir%\%file%bak
-copy %fullfilebak% %fullfile%
+if exist %fullfilebak% (
+  copy %fullfilebak% %fullfile%
+)
 
 

cab8fb6fdd778e920166a21e1ae10fe047fd9011
gforney <gforney@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2015-05-13 20:31:46 +0000

    cfast source/build: determine svn revsion and build time from the get_revision.sh script

--- trunk/cfast/trunk/CFAST/intel_linux_64/make_cfast.sh
+++ trunk/cfast/trunk/CFAST/intel_linux_64/make_cfast.sh
@@ -5,5 +5,9 @@ target=${dir##*/}
 
 source $IFORT_COMPILER/bin/compilervars.sh $platform
 
+source ../scripts/make_frevision.sh ../Source srev.f90
+
 echo Building $target
 make VPATH="../Source:../Include" INCLUDE="../Include" -f ../makefile $target
+
+source ../scripts/restore_frevision.sh ../Source srev.f90

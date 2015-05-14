6983b39a9af4a00022345996bd5ad19e737702ab
cfastdev <cfastdev@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2015-05-14 13:52:11 +0000

    CFAST Source: Correct spelling error

--- cfast/trunk/CFAST/Source/srev.f90
+++ cfast/trunk/CFAST/Source/srev.f90
@@ -3,6 +3,6 @@
     character(len=256), intent(out) :: revision, revision_date, compile_date
     revision = trim(adjustl("unknown"))
     revision_date = trim(adjustl("unknown"))
-    compile_date = trim(adjustl("uknown"))
+    compile_date = trim(adjustl("unknown"))
     return
     end subroutine get_info

26c889093b5b1429df13a0d087f5a411f1f940e0
gforney <gforney@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2015-05-15 22:30:17 +0000

    cfast build: remove wc, minor correction to contract and restore scripts

--- trunk/cfast/trunk/CFAST/scripts/restore_frevision.bat
+++ trunk/cfast/trunk/CFAST/scripts/restore_frevision.bat
@@ -5,6 +5,6 @@ if NOT exist %file% (
   exit /b 1
 )
 
-call ..\scripts\contract_keyword Revision: %file%
-call ..\scripts\contract_keyword RevisionDate: %file%
-call ..\scripts\contract_keyword CompileDate: %file%
+call ..\scripts\contract_keyword Revision %file%
+call ..\scripts\contract_keyword RevisionDate %file%
+call ..\scripts\contract_keyword CompileDate %file%

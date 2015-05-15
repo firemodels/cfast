3d3118ac823309e7c487a0a054e7a79945cebd61
gforney <gforney@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2015-05-15 18:13:32 +0000

    scripts: add a temporary script, expand_keyword3.bat that is a more general version of expand_keyword.bat

--- cfast/trunk/CFAST/scripts/contract_keyword.bat
+++ cfast/trunk/CFAST/scripts/contract_keyword.bat
@@ -6,5 +6,5 @@ if NOT exist %file% (
   exit 1 /b
 )
 
-sed -e "s/$%keyword%.*\$/$%keyword% unknown $/g" %file% | sed "s/$/\r/" > %temp%\temp2.txt
+sed -e "s/$%keyword%.*\$/$%keyword%: unknown $/g" %file% | sed "s/$/\r/" > %temp%\temp2.txt
 copy %temp%\temp2.txt %file% 1> Nul 2>&1

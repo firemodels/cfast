093cbb020384dbbc9dc508c3709017202f79ce66
gforney <gforney@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2015-05-17 14:07:28 +0000

    build scripts: remove Cedit\scripts directory (not needed)

--- cfast/trunk/Cedit/scripts/contract_about.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-:: contract the keyword Revision in About.vb
-:: this script must be called in the directory containing About.vb
-
-call ..\CFAST\scripts\contract_keyword Revision About.vb
--- cfast/trunk/Cedit/scripts/expand_about.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-:: expand the keyword Revision in About.vb
-:: this script must be called in the directory containing About.vb
-
-call ..\CFAST\scripts\get_repo_properties .
-call ..\CFAST\scripts\expand_keyword Revision %revision% About.vb

af4bec7d4112048dbd7f966e6d16a2d952ad8904
gforney <gforney@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2015-05-17 23:14:31 +0000

    build scripts: add support for git in linux keyword expand/contract scripts

--- trunk/cfast/trunk/CFAST/scripts/get_repo_properties.sh
+++ trunk/cfast/trunk/CFAST/scripts/get_repo_properties.sh
@@ -1,44 +1,102 @@
 #!/bin/bash
-svn_dir=$1
+repo_dir=$1
 
 export revision=unknown
 export revision_date=unknown
 export build_date=unknown
+validsvn=0
+validgit=0
+havesvn=1
+havegit=1
 
 CURDIR=`pwd`
 
+# checking if repo_dir exists
+
+if [ ! -e "$repo_dir" ] ; then
+  echo directory $repo_dir does not exist
+  exit
+fi
+
 # looking for svn
 
-notfound=`svn 2>&1 | tail -1 | grep "not found" | wc -l`
+notfound=`svn info 2>&1 | tail -1 | grep "not found" | wc -l`
 if [ "$notfound" == "1" ] ; then
-  exit
+  havesvn=0
 fi
 
-if [ ! -e "$svn_dir" ] ; then
-  echo directory $svn_dir does not exist
-  exit
+# looking for git
+
+notfound=`git 2>&1 | tail -1 | grep "not found" | wc -l`
+if [ "$notfound" == "1" ] ; then
+  havegit=0
 fi
 
-cd $svn_dir
+# warn if both svn and git are not available
 
-# is this a valid svn repository
-
-notworking=`svn 2>&1 | grep "not a working copy" | wc -l`
-if [ "$notworking" == "1" ] ; then
-  echo $svn_dir is not a valid svn repository
-  cd $CURDIR
-  exit
+if [ "$havesvn" == "0" ] ; then
+  if [ "$havesvn" == "0" ] ; then
+    echo "*** warning: neither svn nor git are available"
+    cd $CURDIR
+    exit
+  fi
 fi
 
-# get svn revision number
+# check if repo_dir is a valid svn repository
 
-revision=`svn info 2>&1 | grep "Last Changed Rev:" | awk -F' ' '{print $4}'`
+if [ "$havesvn" == "1" ]; then
+  validsvn=1
+  notworking=`svn info 2>&1 | grep "not a working copy" | wc -l`
+  if [ "$notworking" == "1" ] ; then
+    validsvn=0
+  fi
+fi
 
-# get svn date
+# if not an svn repo check if repo_dir is a valid git repository
 
-revision_date=`svn info 2>&1 | grep "Last Changed Date:" | awk -F" " '{print $4,$5}'`
+if [ "$validsvn" == "0" ] ; then
+  if [ "$havegit" == "1" ] ; then
+    validgit=1
+    notworking=`git log . 2>&1 | grep "Not a git repository" | wc -l`
+    if [ "$notworking" == "1" ] ; then
+      validgit=0
+    fi
+  fi
+fi
 
-# get current date time
+# warn if not a valid repository
+
+if [ "$validsvn" == "0" ] ; then
+  if [ "$validgit" == "0" ] ; then
+    echo "*** warning: $repo_dir is not a valid repository (git or svn)"
+    cd $CURDIR
+    exit
+  fi
+fi
+
+# examine properties of repo_dir 
+
+cd $repo_dir
+
+# get revision number
+
+if [ "$validsvn" == 1 ] ; then
+  revision=`svn info 2>&1 | grep "Last Changed Rev:" | awk -F' ' '{print $4}'`
+fi
+if [ "$validgit" == 1 ] ; then
+  revision=`git log . 2>&1 | head -1 | awk -F " " '{print $2}'`
+fi
+
+# get date/time of latest repository commit
+
+if [ "$validsvn" == 1 ] ; then
+  revision_date=`svn info 2>&1 | grep "Last Changed Date:" | awk -F" " '{print $4,$5}'`
+fi
+if [ "$validgit" == 1 ] ; then
+  revision_date=`git log --date=iso . 2>&1 | head -3 | tail -1 | awk -F" " '{print $2,$3}'`
+fi
+
+# get current date/time
 
 build_date=`date "+%F %T"`
 

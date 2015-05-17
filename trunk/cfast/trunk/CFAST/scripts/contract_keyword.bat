91bb02678a7fe8b8c7e9db06158886ab52f11807
gforney <gforney@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2015-05-17 14:05:12 +0000

    build scripts: add documentation to keyword expanding/contracting scripts

--- trunk/cfast/trunk/CFAST/scripts/contract_keyword.bat
+++ trunk/cfast/trunk/CFAST/scripts/contract_keyword.bat
@@ -2,6 +2,13 @@
 
 :: contract keyword located in file
 
+if "%1" == "" (
+  echo usage: contract_keyword keyword file
+  echo        change all occurences of $keyword: .... $ to
+  echo        $keyword: unknown $ in file
+  goto eof
+)
+
 set keyword=%1
 set file=%2
 
@@ -11,3 +18,5 @@ if NOT exist %file% (
 
 sed -e "s/$%keyword%:.*\$/$%keyword%: unknown $/g" %file% | sed "s/$/\r/" > %temp%\temp2.txt
 copy %temp%\temp2.txt %file% 1> Nul 2>&1
+
+:eof

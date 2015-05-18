1ab19216a3f367fc5e8cf2f722654b5cb7156275
gforney <gforney@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2015-05-18 14:20:58 +0000

    build scripts: corrections to get repo properties script

--- cfast/trunk/CFAST/scripts/get_repo_properties.bat
+++ cfast/trunk/CFAST/scripts/get_repo_properties.bat
@@ -1,10 +1,10 @@
 @echo off
 
-:: obtain various properties of the directory svn_dir
+:: obtain various properties of the directory repo_dir
 :: These properties are the revision and revision date 
 :: of the directory and the current date.
 
-set svn_dir=%1
+set repo_dir=%1
 
 set revision=unknown
 
@@ -20,10 +20,17 @@ set havegit=1
 set validsvn=0
 set validgit=0
 
-set temp1=%temp%\temp.txt
-set temp1c=%temp%\tempc.txt
+set temp1="%temp%\temp.txt"
+set temp1c="%temp%\tempc.txt"
 
 set CURDIR=%CD%
+
+:: check to see if %repo_dir% exists
+
+if NOT exist %repo_dir% (
+  echo *** warning: The directory %repo_dir% does not exist.
+  exit /b 1
+)
 
 :: ----------------- make sure various required software tools are available --------------------------
 
@@ -36,6 +43,27 @@ set /p flag=<%temp1c%
 if %flag% == 1 (
   set havesvn=0
 )
+
+cd %repo_dir%
+
+:: is this a valid svn repository
+
+if %havesvn% == 0 goto skiphavesvn
+  set validsvn=1
+  svn info 1> %temp1% 2>&1
+  type %temp1% | find /i /c "not a working copy" > %temp1c%
+  set glag=
+  set /p glag=<%temp1c%
+  if %glag% == 1 (
+    set svn_revision=invalid
+    cd %CURDIR%
+    set validsvn=0
+    if %havegit% == 0 (
+      echo "*** warning: %repo_dir% is not a valid svn repository"
+      exit /b 1
+    )
+  )
+:skiphavesvn
 
 :: looking for git
 
@@ -51,9 +79,28 @@ if %flag% == 1 (
   )
 )
 
-:: looking for head (only used with git)
+:: is this a valid git repository
 
-if %havegit% == 1 (
+if %validsvn% == 0 (
+  if %havegit% == 1 (
+    set validgit=1
+    git log . 1> %temp1% 2>&1
+    type %temp1% | find /i /c "Not a git repository" > %temp1c%
+    set flag=
+    set /p flag=<%temp1c%
+    if %flag% == 1 (
+      set svn_revision=invalid
+      cd %CURDIR%
+      set validgit=0
+      echo *** warning: %repo_dir% is not a valid git repository
+      exit /b 1
+    ) 
+  )
+)
+
+:: looking for head (used only if this is notonly used with git)
+
+if %validgit% == 1 (
   head -h 1> %temp1% 2>&1
   type %temp1% | find /i /c "not recognized" > %temp1c%
   set flag=
@@ -61,12 +108,12 @@ if %havegit% == 1 (
   if %flag% == 1 (
     echo *** warning: head was not found.
     exit /b 1
-  )
+  ) 
 )
 
 :: looking for tail (only used with git)
 
-if %havegit% == 1 (
+if %validgit% == 1 (
   tail -h 1> %temp1% 2>&1
   type %temp1% | find /i /c "not recognized" > %temp1c%
   set flag=
@@ -88,53 +135,6 @@ if %flag% == 1 (
   exit /b 1
 )
 
-:: check to see if %svn_dir% exists
-
-if NOT exist %svn_dir% (
-  echo *** warning: The directory %svn_dir% does not exist.
-  exit /b 1
-)
-
-cd %svn_dir%
-
-:: is this a valid svn repository
-
-if NOT %havesvn% == 1 goto skiphavesvn
-  set validsvn=1
-  svn info 1> %temp1% 2>&1
-  type %temp1% | find /i /c "not a working copy" > %temp1c%
-  set glag=
-  set /p glag=<%temp1c%
-  if %glag% == 1 (
-    set svn_revision=invalid
-    cd %CURDIR%
-    set validsvn=0
-    if %havegit% == 0 (
-      echo "*** warning: %svn_dir% is not a valid svn repository"
-      exit /b 1
-    )
-  )
-:skiphavesvn
-
-:: is this a valid git repository
-
-if %havegit% == 1 (
-  if %validsvn% == 0 (
-    set validgit=1
-    git log . 1> %temp1% 2>&1
-    type %temp1% | find /i /c "Not a git repository" > %temp1c%
-    set flag=
-    set /p flag=<%temp1c%
-    if %flag% == 1 (
-      set svn_revision=invalid
-      cd %CURDIR%
-      set validgit=0
-      echo *** warning: %svn_dir% is not a valid git repository
-      exit /b 1
-    ) 
-  )
-)
-
 :: ----------------- get properties --------------------------
 
 :: get revision number
@@ -144,7 +144,7 @@ if %validsvn% ==1 (
   set /p revision=<%temp1%
 )
 if %validgit% ==1 (
-  git log . 2>&1 | head -1 | gawk -F" " "{print $2}" > %temp1%
+  git log --abbrev-commit . 2>&1 | head -1 | gawk -F" " "{print $2}" > %temp1%
   set /p revision=<%temp1%
 )
 

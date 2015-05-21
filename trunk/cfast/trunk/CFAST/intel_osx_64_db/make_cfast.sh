d5d22fad7b8e9d6e06058f82857c0c08c0b08f6b
gforney <gforney@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2015-05-21 14:29:33 +0000

    build scripts: make keyword expansion scripts consistent with FDS-SMV repo usage

--- trunk/cfast/trunk/CFAST/intel_osx_64_db/make_cfast.sh
+++ trunk/cfast/trunk/CFAST/intel_osx_64_db/make_cfast.sh
@@ -2,9 +2,11 @@
 platform=intel64
 dir=`pwd`
 target=${dir##*/}
-echo target=$target
-exit
+KWDIR=../../Utilities/keyword
+SDIR=../Source
 source $IFORT_COMPILER/bin/compilervars.sh $platform
 
 echo Building $target
+source $KWDIR/expand_file.sh $KWDIR $SDIR $SDIR/srev.f90
 make VPATH="../Source:../Include" INCLUDE="../Include" -f ../makefile $target
+source $KWDIR/contract_file.sh $KWDIR $SDIR/srev.f90

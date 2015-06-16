e4e3b21e57387ddf80c185be2e6885f5d573e35b
mcgratta <mcgratta@ac19ce91-dc31-0410-8cd1-8f5b667a57d6>
2015-06-16 12:50:39 +0000

    CFASTbot: Add mcgratta to mail list.

--- cfast/trunk/Utilities/cfastbot/cfastbot_email_list.bat
+++ cfast/trunk/Utilities/cfastbot/cfastbot_email_list.bat
@@ -1,6 +1,6 @@
 @echo off
 
 :: Mailing lists for Firebot status report
-set mailToCFAST="gforney@gmail.com, rpeacoc@gmail.com"
+set mailToCFAST="gforney@gmail.com, rpeacoc@gmail.com, mcgratta@gmail.com"
 
 set mailToCFASTDebug="gforney@gmail.com"

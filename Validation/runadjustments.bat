acf9a994c396de34935117080b2bc88df6803720
rpeacoc <rpeacoc@nist.gov>
2015-06-30 15:16:20 -0400

    V&V: remove validation profile files before generating new ones to make sure we're using new ones for a validation check

--- Validation/runadjustments.bat
+++ Validation/runadjustments.bat
@@ -1,4 +1,6 @@
 ..\VandV_Calcs\Release\VandV_Calcs.exe CFAST_Pressure_Correction_Inputs.csv
+del LLNL_Enclosure\LLNL_pressures.csv /Q
 copy pressures.csv LLNL_Enclosure\LLNL_pressures.csv /Y
 ..\VandV_Calcs\Release\VandV_Calcs.exe CFAST_Temperature_Profile_inputs.csv
-copy profiles.csv Steckler_Compartment /Y
+del Steckler_Compartment\profiles.csv /Q
+copy profiles.csv Steckler_Compartment\profiles.csv /Y

3db99d344a4392d58542f37ab88ff872c9fc3583
rpeacoc <rpeacoc@nist.gov>
2015-07-07 14:01:23 -0400

    CFAST Source: Use consistent definition of g (taken from ISO standard)

--- CFAST/Source/prec.f90
+++ CFAST/Source/prec.f90
@@ -3,10 +3,6 @@ module precision_parameters
 ! set important parameters having to do with variable precision and array allocations
  
 implicit none
- 
-character(255), parameter :: precid='$id: prec.f90 968 2013-08-23 13:50:35Z gforney $'
-character(255), parameter :: precrev='$revision: 968 $'
-character(255), parameter :: precdate='$date: 2013-08-23 09:50:35 -0400 (fri, 23 aug 2013) $'
 
 ! precision of "four byte" and "eight byte" reals
 
@@ -25,8 +21,8 @@ real(eb), parameter :: sigma=5.67e-8_eb
 real(eb), parameter :: onethird=1.0_eb/3.0_eb
 real(eb), parameter :: twothirds = 2.0_eb/3.0_eb
 
-!real(eb), parameter :: grav_con=9.80665_eb
-real(eb), parameter :: grav_con=9.8_eb
+real(eb), parameter :: grav_con=9.80665_eb
+!real(eb), parameter :: grav_con=9.8_eb
 real(eb), parameter :: gsqrt=sqrt(grav_con)
 
 real(eb), parameter :: cos45 = sqrt(2.0_eb)/2.0_eb

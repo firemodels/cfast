193e9201c86b356dac1d63e42c9ec7e8c2631641
gforney <gforney@gmail.com>
2015-07-20 09:58:34 -0400

    cfast vv: add error trapping to the bot copy scripts

--- Utilities/cfastbot/copy_files.sh
+++ Utilities/cfastbot/copy_files.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 CURDIR=`pwd`
 repo=$1
+repodir=~/repo
 
-cp ~/$repo/Utilities/cfastbot/*.sh .
+if [ -e $repodir ] ; then
+  cp $repodir/Utilities/cfastbot/*.sh .
+else
+  echo ***Error: The directory $repodir does not exist.  Copy aborted.
+fi

0b3703c76866cd6d5244fa15670b1e43e5c298a5
gforney <gforney@gmail.com>
2015-07-20 10:03:28 -0400

    cfast vv: correction to bot copy scipt

--- Utilities/cfastbot/copy_files.sh
+++ Utilities/cfastbot/copy_files.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 CURDIR=`pwd`
 repo=$1
-repodir=~/repo
+repodir=~/$repo
 
 if [ -e $repodir ] ; then
+  cd $repodir
+  git fetch origin
+  git pull
   cp $repodir/Utilities/cfastbot/*.sh .
 else
   echo ***Error: The directory $repodir does not exist.  Copy aborted.

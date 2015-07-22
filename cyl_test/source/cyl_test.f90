9e760a0bc90f477cd7b713b45aa1bf5f3848fdab
gforney <gforney@gmail.com>
2015-07-21 20:53:31 -0400

    cfast source: move get_flux routine to test program file where it is called

--- cyl_test/source/cyl_test.f90
+++ cyl_test/source/cyl_test.f90
@@ -35,4 +35,43 @@
         endif
       end do
       end program cyl_test
+
+! --------------------------- get_flux -------------------------------------------
+
+    subroutine get_flux(t,temp_cable,temp_shroud,flux_out)
+    
+    use precision_parameters
+    implicit none
+
+    real(eb), intent(in) :: t,temp_cable
+    real(eb), intent(out) :: flux_out,temp_shroud
+
+    real(eb) :: factor, factor2, temp_gas
+
+    if(t>=0.0_eb.and.t<=70.0_eb)then
+        factor = (t-0.0_eb)/70.0_eb
+        factor2 = ((t-0.0_eb)*210.0_eb + (70.0_eb-t)*24.0_eb)/70.0_eb
+        ! else if(t>70.0.and.t<=820.0)then
+    else if(t>70.0_eb)then
+        factor = 1.0_eb
+        factor2 = 210.0_eb
+        ! else if(t>820.0.and.t<=1240.0)then
+        !   factor = ((t-820.0)*0.62 + (1240.0-t)*1.0)/(1240.0-820.0)
+        !   factor2 = ((t-820.0)*150.0 + (1240.0-t)*210.0)/(1240.0-820.0)
+        ! else if(t>1240.0)then
+        !   factor = 0.62
+        !   factor2 = 150.0
+    else
+        factor = 0.0_eb
+        factor2 = 24.0_eb
+    endif
+
+    temp_shroud = kelvin_c_offset + 24.0_eb*(1.0_eb-factor)+480.0_eb*factor
+    temp_gas = factor2 + kelvin_c_offset
+    flux_out = 0.95_eb*sigma*(temp_shroud**4-temp_cable**4)
+    ! flux_out = flux_out + 10*(temp_shroud - temp_cable)
+
+    end subroutine get_flux      
+
+
   

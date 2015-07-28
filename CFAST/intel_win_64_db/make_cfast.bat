5c26f13626bdf45c4b1e67fa95ba5ebfe6f65a2a
gforney <gforney@gmail.com>
2015-07-28 13:28:46 -0400

    cfast build: use preprocessing directives to set revision date, build date and revisino

--- CFAST/intel_win_64_db/make_cfast.bat
+++ CFAST/intel_win_64_db/make_cfast.bat
@@ -2,12 +2,8 @@
 
 :: setup compiler environment
 call ..\scripts\setup_intel_compilers.bat intel64
-set KWDIR=..\..\Utilities\keyword
-set SDIR=..\Source
 
 Title Building debug cfast for 64 bit Windows
 
-call %KWDIR%\expand_file %SDIR% %SDIR%\srev.f90
 make VPATH="../Source:../Include" INCLUDE="../Include" -f ..\makefile intel_win_64_db
-call %KWDIR%\contract_file %SDIR%\srev.f90
 pause

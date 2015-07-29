2b02dbbe76fc6f2a430bc88a2525d968270163aa
gforney <gforney@gmail.com>
2015-07-29 14:03:13 -0400

    cfast build: define SHELL variable for windows builds

--- CFAST/intel_win_64_db/make_cfast.bat
+++ CFAST/intel_win_64_db/make_cfast.bat
@@ -5,5 +5,5 @@ call ..\scripts\setup_intel_compilers.bat intel64
 
 Title Building debug cfast for 64 bit Windows
 
-make VPATH="../Source:../Include" INCLUDE="../Include" -f ..\makefile intel_win_64_db
+make VPATH="../Source:../Include" SHELL="c:\windows\system32\cmd.exe" INCLUDE="../Include" -f ..\makefile intel_win_64_db
 pause

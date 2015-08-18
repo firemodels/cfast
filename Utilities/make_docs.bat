8bfd4f04b730f07c5c07550b84b14dcadad2dada
rpeacoc <rpeacoc@nist.gov>
2015-08-18 15:41:02 -0400

    V&V: Update validaiton guide to include PRISME Door tests.

--- Utilities/make_docs.bat
+++ Utilities/make_docs.bat
@@ -1,11 +1,11 @@
 @echo off
 echo Make Technical Reference Guide
 cd ..\Docs\Tech_Ref
-make_guide.bat
+call make_guide.bat
 
 echo Make Users Guide
 cd ..\Users_Guide
-make_guide.bat
+call make_guide.bat
 
 echo Make Validation Guide
 cd ..\Validation_Guide
@@ -13,7 +13,7 @@ make_guide.bat
 
 echo Make Configuration Guide
 cd ..\Configuration_Guide
-make_guide.bat
+call make_guide.bat
 
 cd ..\..\Utilities
 

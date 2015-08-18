50c11ef855d9a95a1b2dfa3583bae9228a8de013
rpeacoc <rpeacoc@nist.gov>
2015-08-18 11:21:24 -0400

    cfastbot: Add batch file to make configuration guide

---
+++ Docs/Configuration_Guide/make_guide.bat
@@ -0,0 +1,23 @@
+@echo off
+set paper=Configuration_Guide
+
+git describe --long --dirty > gitinfo.txt
+set /p gitrevision=<gitinfo.txt
+echo \newcommand^{\gitrevision^}^{%gitrevision%^} > ..\Bibliography\gitrevision.tex
+
+echo pass 1
+pdflatex -interaction nonstopmode %paper% > %paper%.err
+bibtex %paper% > %paper%.err
+echo pass 2
+pdflatex -interaction nonstopmode %paper% > %paper%.err
+echo pass 3
+pdflatex -interaction nonstopmode %paper% > %paper%.err
+
+find "! LaTeX Error:" %paper%.err
+find "Fatal error" %paper%.err
+find "Error:" %paper%.err
+
+if exist ..\Bibliography\gitrevision.tex erase ..\Bibliography\gitrevision.tex
+echo %paper% build complete
+
+

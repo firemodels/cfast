18124f4b31db1f90e5dfe66b71bfe510aa980b0f
cfastdev <cfast@nist.gov>
2015-08-28 22:34:25 -0400

    V&V: Update scatterplots so small temperature differences don't overwhelm statistics.

--- Utilities/run_everything.bat
+++ Utilities/run_everything.bat
@@ -8,19 +8,23 @@ make VPATH="../Source:../Include" INCLUDE="../Include" -f ..\makefile intel_win_
 copy /Y cfast7_win_64.exe ..\..\bin\cfast.exe
 cd ..\..\Utilities
 echo Running validation cases
+Title Running Validation Cases
 cd ..\Validation
 call runall.bat ALL
 call runadjustments.bat
 echo Running verification cases
+Title Running Verification Cases
 cd ..\Verification
 call runall.bat ALL
 echo Running matlab plotting analysis
 cd ..\Utilities\matlab\scripts
 SpeciesMassTestCases
 cd ..\
+Title Running Matlab Plotting Analysis
 Validation
 Verification
 echo Creating Documentation
+Title Creating Documentation
 cd ..\
 call make_docs.bat
 echo.| time

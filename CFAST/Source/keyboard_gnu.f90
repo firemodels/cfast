dd351d450e1d261caea1ab4493acffa798c9bfab
cfastdev <cfast@nist.gov>
2015-10-30 19:11:02 -0400

    CFAST: Fix command parsing for gnu fortran

--- CFAST/Source/keyboard_gnu.f90
+++ CFAST/Source/keyboard_gnu.f90
@@ -1,10 +1,10 @@
 
-! --------------------------- grabky -------------------------------------------
+    ! --------------------------- grabky -------------------------------------------
 
     subroutine grabky (ich,it)
 
     implicit none
-    
+
     integer(2), intent(out) :: ich, it
 
     ich = 0
@@ -13,12 +13,12 @@
     return
     end subroutine grabky
 
-! ---------------------------  -------------------------------------------
+    ! ---------------------------  -------------------------------------------
 
     integer function splitpathqq(path,drive,dir,name,ext)
     character*(*), intent(IN) :: path
     character*(*), intent(out) :: drive, dir, name, ext
-   
+
     character*1 dirsep
     integer :: i, len, first, next
 
@@ -32,36 +32,46 @@
     if(len.eq.0)return
     ! look for first non-blank character
     do i = 1, len
-       first=i
-       if(path(i:i).ne.' ')exit
+        first=i
+        if(path(i:i).ne.' ')exit
     end do
 
     ! look for extension
     next=len
     do i = next, first, -1
-       if(path(i:i)=='.')then
-          ext=path(i:len)
-          next=i-1
-          exit
-       endif
+        if(path(i:i)=='.')then
+            if(path(i+1:i+1)==dirsep)then
+                ext=''
+                next=len
+            else
+                ext=path(i:len)
+                next=i-1
+            endif
+            exit
+        endif
     end do
 
     ! look for filename
     do i = next, first, -1
-       if(path(i:i)==dirsep)then
-          name=path(i+1:next)
-          next=i
-          exit
-       endif
+        if(path(i:i)==dirsep)then
+            name=path(i+1:next)
+            next=i
+            dir=path(first:next)
+            exit
+        endif
+        if (i==first) then
+            name = path(first:next)
+            next=first
+            dir =''
+            exit
+        endif
     end do
-    ! directory
-    dir=path(first:next)
 
-! debug print
-!    write(6,*)"path=",trim(path)
-!    write(6,*)"drive=",trim(drive)
-!    write(6,*)"dir=",trim(dir)
-!    write(6,*)"name=",trim(name)
-!    write(6,*)"ext=",trim(ext)
-!    stop    
+    ! debug print
+    !write(*,*)"path=",trim(path)
+    !write(*,*)"drive=",trim(drive)
+    !write(*,*)"dir=",trim(dir)
+    !write(*,*)"name=",trim(name)
+    !write(*,*)"ext=",trim(ext)
+    !    stop
     end function splitpathqq

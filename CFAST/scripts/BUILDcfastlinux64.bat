280949fa2f43ff3631b1d4065145f19186788045
gforney <gforney@gmail.com>
2015-11-01 15:04:37 -0500

    build scripts: add linux and osx build scripts to web page

--- CFAST/scripts/BUILDcfastlinux64.bat
+++ CFAST/scripts/BUILDcfastlinux64.bat
@@ -1,11 +1,11 @@
 @echo off
-Title Building Smokeview for 64 bit Linuux
+Title Building cfast for 64 bit Linuux
 
 Rem  Windows batch file to build a release Smokeview for Linux 64.
 
 Rem setup environment variables (defining where repository resides etc) 
 
-set envfile="%userprofile%"\fds_smv_env.bat
+set envfile="%userprofile%"\cfast_env.bat
 IF EXIST %envfile% GOTO endif_envexist
 echo ***Fatal error.  The environment setup file %envfile% does not exist. 
 echo Create a file named %envfile% and use SMV/scripts/fds_smv_env_template.bat
@@ -20,14 +20,9 @@ goto:eof
 call %envfile%
 
 %svn_drive%
-cd %svn_root%\smv\scripts
-set version=%smv_version%_%smv_revision%
+set scriptdir=%linux_svn_root%/CFAST/scripts
 
-set scriptdir=FDS-SMV/SMV/scripts
-set bundledir=FDS-SMV/SMV/for_bundle
-set bindir=FDS-SMV/SMV/bin
-
-plink %svn_logon% %scriptdir%/ssh_command.csh %linux_hostname% %scriptdir% MAKEsmvlinux64.sh
+plink %svn_logon% %scriptdir%/ssh_command.csh %linux_hostname% %scriptdir% MAKEcfastlinux64.sh
 
 echo.
 echo compilation complete

b9a46c4e25544536ab2fef6752c8117a7c68d902
gforney <gforney@gmail.com>
2015-11-06 22:09:59 -0500

    cfastbot: call make_xxx to make various components rather than using make command (avoid duplication)

--- VandV_Calcs/intel_win_64/make_vv.bat
+++ VandV_Calcs/intel_win_64/make_vv.bat
@@ -1,9 +1,14 @@
 @echo off
+set arg1=%1
 
 :: setup compiler environment
+if x%arg1% == xbot goto skip1
 call ..\..\CFAST\scripts\setup_intel_compilers.bat intel64
+:skip1
 
-Title Building cfast for 64 bit Windows
+Title VV app for 64 bit Windows
 
 make VPATH=".." -f ..\makefile intel_win_64
+if x%arg1% == xbot goto skip2
 pause
+:skip2

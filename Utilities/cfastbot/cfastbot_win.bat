804e988204bd1b8e672afa7720b3fcfb12838971
gforney <gforney@gmail.com>
2015-12-02 20:32:22 -0500

    cfastbot: correct call to script that builds smokeview

--- Utilities/cfastbot/cfastbot_win.bat
+++ Utilities/cfastbot/cfastbot_win.bat
@@ -360,7 +360,7 @@ echo             debug
 
 cd %FDSroot%\SMV\Build\intel_win_64
 erase *.obj *.mod *.exe smokeview_win_64_db.exe 1> %OUTDIR%\stage2a.txt 2>&1
-call make_smv_db bot 1>> %OUTDIR%\stage2a.txt 2>&1
+call make_smv_db -r bot 1>> %OUTDIR%\stage2a.txt 2>&1
 
 call :does_file_exist smokeview_win_64_db.exe %OUTDIR%\stage2a.txt|| exit /b 1
 call :find_smokeview_warnings "warning" %OUTDIR%\stage2a.txt "Stage 2a"
@@ -369,7 +369,7 @@ echo             release
 
 cd %FDSroot%\SMV\Build\intel_win_64
 erase *.obj *.mod smokeview_win_64.exe 1> %OUTDIR%\stage2b.txt 2>&1
-call make_smv bot 1>> %OUTDIR%\stage2b.txt 2>&1
+call make_smv -r bot 1>> %OUTDIR%\stage2b.txt 2>&1
 set smokeview=%FDSroot%\SMV\Build\intel_win_64\smokeview_win_64.exe
 
 call :does_file_exist smokeview_win_64.exe %OUTDIR%\stage2b.txt|| exit /b 1

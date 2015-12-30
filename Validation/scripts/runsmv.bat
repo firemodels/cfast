040f04e72b7a9cc4d3002e41debf0e3ab9352abf
gforney <gforney@gmail.com>
2015-12-29 22:54:33 -0500

    cfastbot: add debug output to runsmv

--- Validation/scripts/runsmv.bat
+++ Validation/scripts/runsmv.bat
@@ -5,5 +5,7 @@ call %cfastroot%\Validation\Scripts\getopts.bat %*
 set fulldir=%BASEDIR%/%dir%
 
 cd %fulldir%
-echo %infile%
+echo fulldir=%fulldir%
+echo infile=%infile%
+echo smokeview=%smokeview%
 %smokeview% -runscript %infile%

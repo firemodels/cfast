0eca754d74deda4f405f0641998c0e0bc5c6975e
rpeacoc <rpeacoc@nist.gov>
2016-01-05 13:12:42 -0500

    V&V: Update matlab scripts to be consistent with latest FDS revisions

--- Utilities/Matlab/scripts/dataplot.m
+++ Utilities/Matlab/scripts/dataplot.m
@@ -30,7 +30,8 @@
 % Dependencies:
 %    ../scripts/define_drow_variables.m
 %    dvcread.m
-%    parse.m
+%    parsepipe.m
+%    parseplus.m
 %
 % Example: From the command line within the Matlab/functions/ directory,
 %    type
@@ -133,12 +134,13 @@ for i=2:n_plots
     
     if itest && (dtest || otest)
 
+        close all
         figure
         
         define_drow_variables
         
         % Save for scatter plots
-        Q1                      = parse(Quantity);
+        Q1                      = parsepipe(Quantity);
         Save_Quantity(i,1:length(Q1))        = Q1;
         Save_Group_Style(i)     = Group_Style;
         Save_Fill_Color(i)      = Fill_Color;
@@ -155,9 +157,9 @@ for i=2:n_plots
            continue
         end
         [H M] = dvcread(d1_Filename,d1_Col_Name_Row);
-        R1 = parse(d1_Ind_Col_Name);
-        S1 = parse(d1_Dep_Col_Name);
-        style = parse(d1_Style);
+        R1 = parsepipe(d1_Ind_Col_Name);
+        S1 = parsepipe(d1_Dep_Col_Name);
+        style = parsepipe(d1_Style);
         % Wrap entire d1 dataplot routine in try loop
         % Skips case upon any Matlab error
         try
@@ -241,7 +243,8 @@ for i=2:n_plots
                 end
             end
         catch
-            display(['Error: Problem with dataplot row ', num2str(i), ' (', Dataname, '); check syntax of analytical/expected/experimental (d1) columns. Skipping case.'])
+            display(['Error: Problem with dataplot row ', num2str(i), ' (', Dataname,...
+                '); check syntax of analytical/expected/experimental (d1) columns. Skipping case.'])
             continue
         end
         
@@ -251,59 +254,70 @@ for i=2:n_plots
            continue
         end
         [H M] = dvcread(d2_Filename,d2_Col_Name_Row);
-        R2 = parse(d2_Ind_Col_Name);
-        S2 = parse(d2_Dep_Col_Name);
-        style = parse(d2_Style);
+        R2 = parsepipe(d2_Ind_Col_Name);
+        S2 = parsepipe(d2_Dep_Col_Name);
+        style = parsepipe(d2_Style);
         % Wrap entire d2 dataplot routine in try loop
         % Skips case upon any Matlab error
         try
             for j=1:length(S2)
+                
+                % check for "+" operator on columns 
+                SP = parseplus(S2(j));
+
                 d2_Ind_Col = find(strcmp(H,R2(min(j,length(R2)))));
-                d2_Dep_Col = find(strcmp(H,S2(j)));
+                for jj=1:length(SP)
+                    d2_Dep_Col(jj) = find(strcmp(H,SP(jj)));
+                end
                 clear indices
+
                 % Clear flag for stat_x_y metric
                 using_stat_x_y = 0;
                 using_stat_x_y_check_zero = 0;
                 indices = find(d2_Comp_Start<=M(:,d2_Ind_Col) & M(:,d2_Ind_Col)<=d2_Comp_End);
+
+                M_Ind = M(indices,d2_Ind_Col);
+                M_Dep = sum(M(indices,d2_Dep_Col),2);
+
                 if strcmp(Metric,'max')
-                    Save_Predicted_Metric(i,j,1) = max(M(indices,d2_Dep_Col))-d2_Initial_Value;
+                    Save_Predicted_Metric(i,j,1) = max(M_Dep)-d2_Initial_Value;
                 elseif strcmp(Metric,'min')
-                    Save_Predicted_Metric(i,j,1) = d2_Initial_Value-min(M(indices,d2_Dep_Col));
+                    Save_Predicted_Metric(i,j,1) = d2_Initial_Value-min(M_Dep);
                 elseif strcmp(Metric,'maxabs')
-                    Save_Predicted_Metric(i,j,1) = max(abs(M(indices,d2_Dep_Col)-d2_Initial_Value));
+                    Save_Predicted_Metric(i,j,1) = max(abs(M_Dep-d2_Initial_Value));
                 elseif strfind(Metric,'max_')
                     using_stat_x_y = 1;
                     compare_indices = sscanf(Metric, ['max_' '%f' '_' '%f']);
                     if compare_indices(2) == j
-                        Save_Predicted_Metric(i,1,1) = max(M(indices,d2_Dep_Col))-d2_Initial_Value;
+                        Save_Predicted_Metric(i,1,1) = max(M_Dep)-d2_Initial_Value;
                         using_stat_x_y_check_zero = 1;
                     end
                 elseif strcmp(Metric,'mean')
-                    Save_Predicted_Metric(i,j,1) = abs(mean(M(indices,d2_Dep_Col))-d2_Initial_Value);
+                    Save_Predicted_Metric(i,j,1) = abs(mean(M_Dep)-d2_Initial_Value);
                 % If mean_x_y is specified for a plot with multiple curves,
                 % then get the results from curve y only
                 elseif strfind(Metric,'mean_')
                     using_stat_x_y = 1;
                     compare_indices = sscanf(Metric, ['mean_' '%f' '_' '%f']);
                     if compare_indices(2) == j
-                        Save_Predicted_Metric(i,1,1) = abs(mean(M(indices,d2_Dep_Col))-d2_Initial_Value);
+                        Save_Predicted_Metric(i,1,1) = abs(mean(M_Dep)-d2_Initial_Value);
                         using_stat_x_y_check_zero = 1;
                     end
                 elseif strcmp(Metric,'all')
-                    Save_Predicted_Metric(i,j,1:length(indices)) = M(indices,d2_Dep_Col)-d2_Initial_Value;
+                    Save_Predicted_Metric(i,j,1:length(indices)) = M_Dep-d2_Initial_Value;
                 elseif strcmp(Metric,'threshold')
-                    Save_Predicted_Metric(i,j,1) = min(M(indices,d2_Dep_Col))-d2_Initial_Value;
+                    Save_Predicted_Metric(i,j,1) = min(M_Dep)-d2_Initial_Value;
                 elseif strcmp(Metric,'area')
-                    Save_Predicted_Metric(i,j,1) = trapz(M(indices,d2_Ind_Col), M(indices,d2_Dep_Col))-d2_Initial_Value;
+                    Save_Predicted_Metric(i,j,1) = trapz(M_Ind,M_Dep)-d2_Initial_Value;
                 elseif strcmp(Metric,'end')
-                    Save_Predicted_Metric(i,j,1) = M(indices(end),d2_Dep_Col)-d2_Initial_Value;
+                    Save_Predicted_Metric(i,j,1) = M_Dep(end)-d2_Initial_Value;
                 % If end_x_y is specified for a plot with multiple curves,
                 % then get the results from curve y only
                 elseif strfind(Metric,'end_')
                     using_stat_x_y = 1;
                     compare_indices = sscanf(Metric, ['end_' '%f' '_' '%f']);
                     if compare_indices(2) == j
-                        Save_Predicted_Metric(i,1,1) = M(indices(end),d2_Dep_Col)-d2_Initial_Value;
+                        Save_Predicted_Metric(i,1,1) = M_Dep(end)-d2_Initial_Value;
                         using_stat_x_y_check_zero = 1;
                     end
                 else
@@ -318,13 +332,18 @@ for i=2:n_plots
                     Save_Predicted_Metric(i,1,1) = 1E-12;
                 end
                 clear indices
+
+                % Plots
                 indices = find(d2_Start<=M(:,d2_Ind_Col) & M(:,d2_Ind_Col)<=d2_End);
+                M_Ind = M(indices,d2_Ind_Col);
+                M_Dep = sum(M(indices,d2_Dep_Col),2);
+                clear d2_Dep_Col;
                 if strcmp(Flip_Axis,'no')
-                    X = M(indices,d2_Ind_Col)/Scale_Ind;
-                    Y = M(indices,d2_Dep_Col)/Scale_Dep;
+                    X = M_Ind/Scale_Ind;
+                    Y = M_Dep/Scale_Dep;
                 else
-                    X = M(indices,d2_Dep_Col)/Scale_Dep;
-                    Y = M(indices,d2_Ind_Col)/Scale_Ind;
+                    X = M_Dep/Scale_Dep;
+                    Y = M_Ind/Scale_Ind;
                 end
                 if strcmp(Plot_Type,'linear')
                     K(length(S1)+j) = plot(X,Y,char(style(j)));
@@ -335,16 +354,18 @@ for i=2:n_plots
                 elseif strcmp(Plot_Type,'semilogy')
                     K(length(S1)+j) = semilogy(X,Y,char(style(j)));
                 end
+
             end
         catch
-            display(['Error: Problem with dataplot row ', num2str(i), ' (', Dataname, '); check syntax of FDS/model results (d2) columns. Skipping case.'])
+            display(['Error: Problem with dataplot row ', num2str(i), ' (', Dataname,...
+                '); check syntax of FDS/model results (d2) columns. Skipping case.'])
             continue
         end
         hold off
         
         % Wrap entire plot/save routine in try loop
         % Skips case upon any Matlab error
-
+        try
             if strcmp(Plot_Type,'linear') & strcmp(Flip_Axis,'no')
                 X_Title_Position = Min_Ind+Title_Position(1)*(Max_Ind-Min_Ind);
                 Y_Title_Position = Min_Dep+Title_Position(2)*(Max_Dep-Min_Dep);
@@ -372,7 +393,7 @@ for i=2:n_plots
             end
 
             set(gca,'FontName',Font_Name)
-            set(gca,'FontSize',Label_Font_Size)
+            set(gca,'FontSize',Label_Font_Size) 
 
             if strcmp(Flip_Axis,'no')
                 xlabel(Ind_Title,'Interpreter',Font_Interpreter,'FontSize',Label_Font_Size)
@@ -387,16 +408,19 @@ for i=2:n_plots
                 text(X_Title_Position,Y_Title_Position,...
                     Plot_Title,'FontSize',Title_Font_Size,'FontName',Font_Name,'Interpreter',Font_Interpreter)
             end
+
             if size(Key_Position)>0
-                legend_handle = legend(K,[parse(d1_Key),parse(d2_Key)],'Location',Key_Position);
-                if strcmp(Key_Position,'EastOutside')
-                   pos = get(legend_handle,'position');
-                   set(legend_handle,'position',[Paper_Width (Plot_Y+(Plot_Height-pos(4))/2) pos(3:4)])
-                end
-                if strcmp(Key_Position,'SouthEastOutside')
-                   pos = get(legend_handle,'position');
-                   set(legend_handle,'position',[Paper_Width Plot_Y pos(3:4)])
-                end
+                legend_handle = legend(K,[parsepipe(d1_Key),parsepipe(d2_Key)],'Location',Key_Position);
+                % % The latest version of Matlab (R2015b) apparently get this correct, but I will
+                % % leave this commented code for a bit until we are sure this is working on blaze.
+                % if strcmp(Key_Position,'EastOutside')
+                %    pos = get(legend_handle,'position');
+                %    set(legend_handle,'position',[Paper_Width (Plot_Y+(Plot_Height-pos(4))/2) pos(3:4)])
+                % end
+                % if strcmp(Key_Position,'SouthEastOutside')
+                %    pos = get(legend_handle,'position');
+                %    set(legend_handle,'position',[Paper_Width Plot_Y pos(3:4)])
+                % end
                 set(legend_handle,'Interpreter',Font_Interpreter);
                 set(legend_handle,'Fontsize',Key_Font_Size);
                 set(legend_handle,'Box','on');
@@ -425,10 +449,14 @@ for i=2:n_plots
             set(gcf,'Visible',Figure_Visibility);
             set(gcf,'PaperUnits',Paper_Units);
             set(gcf,'PaperSize',[PDF_Paper_Width Paper_Height]);
-            set(gcf,'PaperPosition',[0 0 PDF_Paper_Width Paper_Height]); 
+            set(gcf,'PaperPosition',[0 0 PDF_Paper_Width Paper_Height]);
             display(['Printing plot ',num2str(i),'...'])
             print(gcf,Image_File_Type,[Manuals_Dir,Plot_Filename])
-
+        catch
+            display(['Error: Problem with dataplot row ', num2str(i), ' (', Dataname,...
+                '); check syntax of plot/save settings. Skipping case.'])
+            continue
+        end    
         
     end
     clear S1 S2 K style H M X Y P parameters
--- Utilities/Matlab/scripts/plot_style.m
+++ Utilities/Matlab/scripts/plot_style.m
@@ -2,7 +2,18 @@
 % 7-16-2009
 % plot_style.m
 %
-% Preferred style for FDS and Smokeview plots 
+% Preferred style for FDS and Smokeview plots
+%
+% Usage: When creating a new figure use the following:
+%
+% figure
+% plot_style
+% set(gca,'FontName',Font_Name)
+% set(gca,'FontSize',Label_Font_Size)
+% set(gcf,'Visible',Figure_Visibility);
+% set(gcf,'PaperUnits',Paper_Units);
+% set(gcf,'PaperSize',[Paper_Width Paper_Height]);
+% set(gcf,'PaperPosition',[0 0 Paper_Width Paper_Height]);
 
 % Font properties
 Font_Name = 'Times';
@@ -18,7 +29,7 @@ Marker_Size = 4;
 Line_Width      = 1.0;
 
 % Plot properties
-Plot_Units      = 'normalized'; %'inches';
+Plot_Units      = 'inches';
 Plot_Width      = 5.0;
 Plot_Height     = 3.4;
 Plot_X          = 1.2;
@@ -40,19 +51,3 @@ Scat_Paper_Width  = 6.0;
 % Print properties
 Figure_Visibility = 'on';
 Image_File_Type = '-dpdf';
-
-% SVN text position
-SVN_Scale_X = 0.80;
-SVN_Scale_Y = 1.05;
-
-% Set default figure properties
-set(0,'DefaultAxesUnits',Plot_Units)
-set(0,'DefaultAxesPosition',[Plot_X/Paper_Width,Plot_Y/Paper_Height,Plot_Width/Paper_Width,Plot_Height/Paper_Height])
-set(0,'DefaultTextInterpreter',Font_Interpreter)
-set(0,'DefaultAxesFontName',Font_Name)
-set(0,'DefaultLineLineWidth',Line_Width)
-set(0,'DefaultAxesFontSize',Label_Font_Size)
-set(0,'DefaultTextFontSize',Label_Font_Size)
-set(0,'DefaultFigurePaperUnits',Paper_Units);
-set(0,'DefaultFigurePaperSize',[Paper_Width Paper_Height]);
-set(0,'DefaultFigurePaperPosition',[0 0 Paper_Width Paper_Height]);

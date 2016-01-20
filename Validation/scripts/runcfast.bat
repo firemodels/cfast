8389d5086e851eafafb0d38c91e646a6dcc2badf
gforney <gforney@gmail.com>
2016-01-20 14:43:40 -0500

    cfastbot: add name of exe when running cfast

--- Validation/scripts/runcfast.bat
+++ Validation/scripts/runcfast.bat
@@ -4,10 +4,11 @@ call %SVNROOT%\Validation\Scripts\getopts.bat %*
 
 set fulldir=%BASEDIR%/%dir%
 
+
 set in=%infile%
 set out=%infile%.err
 set stopfile=%infile%.stop
 
 cd %fulldir%
-echo %in% started
+echo %in% started using %CFAST%
 %CFAST% %in%  1> %out% 2>&1 -V

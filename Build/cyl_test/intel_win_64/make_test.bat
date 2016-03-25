cdb464b1f0828093c6b233d63fd879167908db9f
rpeacoc <rpeacoc@gmail.com>
2016-03-25 14:47:20 -0400

    scripts: Move make files for cyl_test to new Build folder

--- Build/cyl_test/intel_win_64/make_test.bat
+++ Build/cyl_test/intel_win_64/make_test.bat
@@ -5,5 +5,5 @@ call ..\..CFAST\scripts\setup_intel_compilers.bat intel64
 
 Title Building cfast for 64 bit Windows
 
-make VPATH="../source:../../Source/CFAST/Source" -f ..\makefile intel_win_64
+make -f ..\makefile intel_win_64
 pause

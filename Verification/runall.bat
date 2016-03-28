d6c717982da70fffd597a19d03cd2f50f16fd333
rpeacoc <rpeacoc@nist.gov>
2016-03-28 09:53:32 -0400

    repo org: move for_bundle folder to Utilities

--- Verification/runall.bat
+++ Verification/runall.bat
@@ -7,48 +7,48 @@ call cleanall.bat
 echo Running Energy Balance cases
 cd Energy_Balance
 call ..\..\Validation\cleancfast.bat
-background -u 98 ..\..\for_bundle\Bin\cfast sealed_test -V
-background -u 98 ..\..\for_bundle\Bin\cfast sealed_test_2_layers -V
+background -u 98 ..\..\Utilities\for_bundle\Bin\cfast sealed_test -V
+background -u 98 ..\..\Utilities\for_bundle\Bin\cfast sealed_test_2_layers -V
 cd ..
 
 echo Running Mass Balance cases
 cd Mass_Balance
 call ..\..\Validation\cleancfast.bat
-background -u 98 ..\..\for_bundle\Bin\cfast species_mass_1 -V
-background -u 98 ..\..\for_bundle\Bin\cfast species_mass_2 -V
-background -u 98 ..\..\for_bundle\Bin\cfast species_mass_3 -V
-background -u 98 ..\..\for_bundle\Bin\cfast species_mass_4 -V
+background -u 98 ..\..\Utilities\for_bundle\Bin\cfast species_mass_1 -V
+background -u 98 ..\..\Utilities\for_bundle\Bin\cfast species_mass_2 -V
+background -u 98 ..\..\Utilities\for_bundle\Bin\cfast species_mass_3 -V
+background -u 98 ..\..\Utilities\for_bundle\Bin\cfast species_mass_4 -V
 cd ..
 
 echo Running Thermal Equilibrium cases
 cd Thermal_Equilibrium
 call ..\..\Validation\cleancfast.bat
-background -u 98 ..\..\for_bundle\Bin\cfast basic_tempequilib -V
-background -u 98 ..\..\for_bundle\Bin\cfast basic_tempequilib_window -V
-background -u 98 ..\..\for_bundle\Bin\cfast basic_tempequilib_window_elevation -V
+background -u 98 ..\..\Utilities\for_bundle\Bin\cfast basic_tempequilib -V
+background -u 98 ..\..\Utilities\for_bundle\Bin\cfast basic_tempequilib_window -V
+background -u 98 ..\..\Utilities\for_bundle\Bin\cfast basic_tempequilib_window_elevation -V
 cd ..
 
 echo Running Ventilation cases
 cd Ventilation
 call ..\..\Validation\cleancfast.bat
-background -u 98 ..\..\for_bundle\Bin\cfast ventilation_1 -V
-background -u 98 ..\..\for_bundle\Bin\cfast ventilation_2 -V
-background -u 98 ..\..\for_bundle\Bin\cfast ventilation_3 -V
-background -u 98 ..\..\for_bundle\Bin\cfast ventilation_4 -V
-background -u 98 ..\..\for_bundle\Bin\cfast VVent_Tests -V
+background -u 98 ..\..\Utilities\for_bundle\Bin\cfast ventilation_1 -V
+background -u 98 ..\..\Utilities\for_bundle\Bin\cfast ventilation_2 -V
+background -u 98 ..\..\Utilities\for_bundle\Bin\cfast ventilation_3 -V
+background -u 98 ..\..\Utilities\for_bundle\Bin\cfast ventilation_4 -V
+background -u 98 ..\..\Utilities\for_bundle\Bin\cfast VVent_Tests -V
 cd ..
 
 echo Running Sprinkler cases
 cd Sprinkler
 call ..\..\Validation\cleancfast.bat
-background -u 98 ..\..\for_bundle\Bin\cfast sprinkler_1 -V
+background -u 98 ..\..\Utilities\for_bundle\Bin\cfast sprinkler_1 -V
 cd ..
 
 echo Running Radiation cases
 cd Radiation
 call ..\..\Validation\cleancfast.bat
-background -u 98 ..\..\for_bundle\Bin\cfast radiation_1 -V
-background -u 98 ..\..\for_bundle\Bin\cfast radiation_2 -V
+background -u 98 ..\..\Utilities\for_bundle\Bin\cfast radiation_1 -V
+background -u 98 ..\..\Utilities\for_bundle\Bin\cfast radiation_2 -V
 cd ..
 
 echo Waiting for all CFAST runs to finish

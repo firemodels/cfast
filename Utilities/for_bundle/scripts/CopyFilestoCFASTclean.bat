6ab0c2046c86d19c2f2d62fa44406a5829dfc6d1
rpeacoc <rpeacoc@nist.gov>
2016-03-28 16:05:03 -0400

    bundle: Update bundle scripts to correct a missing file

--- Utilities/for_bundle/scripts/CopyFilestoCFASTclean.bat
+++ Utilities/for_bundle/scripts/CopyFilestoCFASTclean.bat
@@ -1,24 +1,24 @@
-@echo off
+:: @echo off
 
 set cfastroot=c:\Users\rpeacoc\cfastgitclean
 
 @echo *** Copying CFAST Executables
-copy ..\Bin\CFAST.exe %cfastroot%\bin /Y
-copy ..\Bin\CEdit.exe %cfastroot%\bin /Y
+copy ..\Bin\CFAST.exe %cfastroot%\Utilities\for_bundle\Bin /Y
+copy ..\Bin\CEdit.exe %cfastroot%\Utilities\for_bundle\Bin /Y
 
 @echo *** Copying Smokeview executables
 
-if NOT exist %cfastroot%\SMV6 (
-   mkdir %cfastroot%\SMV6
+if NOT exist %cfastroot%\Utilities\for_bundle\SMV6 (
+   mkdir %cfastroot%\Utilities\for_bundle\SMV6
 )
-copy ..\SMV6\*.* %cfastroot%\SMV6\ /Y
-if NOT exist %cfastroot%\SMV6\textures (
-   mkdir %cfastroot%\SMV6\textures
+copy ..\SMV6\*.* %cfastroot%\Utilities\for_bundle\SMV6\ /Y
+if NOT exist %cfastroot%\Utilities\for_bundle\SMV6\textures (
+   mkdir %cfastroot%\Utilities\for_bundle\SMV6\textures
 )
-copy ..\SMV6\textures\*.* %cfastroot%\SMV6\textures\ /Y
+copy ..\SMV6\textures\*.* %cfastroot%\Utilities\for_bundle\SMV6\textures\ /Y
 
 @echo *** copying DLLs
-copy ..\bin\C1*.dll %cfastroot%\bin /Y
+copy ..\Utilities\for_bundle\Bin\C1*.dll %cfastroot%\bin /Y
 
 @echo *** copy install utilities
-copy \users\rpeacoc\FIRE-LOCAL\repo_exes\set_path.exe %cfastroot%\scripts\bundleinfo\ /Y
+copy \users\rpeacoc\FIRE-LOCAL\repo_exes\set_path.exe %cfastroot%\Utilities\for_bundle\scripts\bundleinfo\ /Y

b331ea8549fe570d0913755af14f6c41750dd53a
rpeacoc <rpeacoc@nist.gov>
2016-08-24 13:22:05 -0400

    V&V: Revise species Matlab scripts to use new species mass spreadsheets (i.e., actual CFAST-calculated masses) rather than now unneeded calculations.

--- Utilities/Matlab/scripts/csvreaderALL.m
+++ Utilities/Matlab/scripts/csvreaderALL.m
@@ -1,170 +1,25 @@
-function [Time,mCO2vectorFIN,mH2OvectorFIN,TotH2Omass,TotCO2mass,mCO2vector,mH2Ovector] = csvreaderALL(filename)
+function [Time,mCO2,mH2O] = csvreaderALL(filename,Comp)
+[H D] = dvcread(filename,1);
 
-Po = 101325;%Pa
-R = 8.314;%kJ/(mol*K)
-MO2 = 32.00;%g/mol
-MCO2 = 44.01;%g/mol
-MH2O = 18.01528;%g/mol
-MN2 = 28.02;%g/mol
-MC = 12.01;%g/mol
-MH = 1.008;%g/mol
-MO = 16.00;%g/mol
-gamma = 1.4;
-Cp = 1012;
+colTime = find(strncmpi(H,'Time',4));
+colO2u = find(strncmpi(H,strcat('ULO2',Comp),6));
+colCO2u = find(strncmpi(H,strcat('ULCO2',Comp),7));
+colH2Ou = find(strncmpi(H,strcat('ULH2O',Comp),7));
+colO2l = find(strncmpi(H,strcat('LLO2',Comp),6));
+colCO2l = find(strncmpi(H,strcat('LLCO2',Comp),7));
+colH2Ol = find(strncmpi(H,strcat('LLH2O',Comp),7));
 
-filenameN = strcat(filename,'_n.csv');
-Q = importdata(filenameN,',',4);
-F = strsplitter(Q.textdata{1,1},',');
+Time = D(:,colTime)/60;
+CO2u = D(:,colCO2u);
+H2Ou = D(:,colH2Ou);
+CO2l = D(:,colCO2l);
+H2Ol = D(:,colH2Ol);
 
-colTu = find(strncmpi(F,'ULT',3));
-colVu = find(strncmpi(F,'VOL',3));
-colTl = find(strncmpi(F,'LLT',3));
-colLH = find(strncmpi(F,'HGT',3));
-colTime = find(strcmp(F,'Time'));
-colP = find(strncmpi(F,'PRS',3));
-numComp = length(colTu);
-dim = size(Q.data);
-a = dim(1);
-Time = zeros(dim(1),colTime);
-Tu = zeros(dim(1),numComp);
-P = zeros(dim(1),numComp);
-Vu = zeros(dim(1),numComp);
-Tl = zeros(dim(1),numComp);
-Vl = zeros(dim(1),numComp);
-LH = zeros(dim(1),numComp);
-indA = isempty(colLH);
-indB = isempty(colTl);
-for i = 1:dim(1)
-    Time(i) = Q.data(i,colTime);
+mCO2 = CO2u;
+mH2O = H2Ou;
+if size(CO2u) == size(CO2l) % if it's a one zone case, there is no lower layer
+    mCO2 = CO2u + CO2l;
+    mH2O = H2Ou + H2Ol;
 end
-Time = Time*(1/60);
-
-for i = 1:numComp
-for j = 1:dim(1)    
-Tu(j,i) = Q.data(j,colTu(i)) + 273.15;%K
-P(j,i) = Q.data(j,colP(i)) + Po;%Pa
-Vu(j,i) = Q.data(j,colVu(i));%m^3
-end
-end
-
-for i = 1:numComp
-    if indA == 1 && indB == 1
-        break
-    end
-for j = 1:dim(1)
-Vl(j,i) = Vu(j,i)*Q.data(j,colLH(i))/(Q.data(1,colLH(i)) - Q.data(j,colLH(i)));%m^3
-Tl(j,i) = Q.data(j,colTl(i)) + 273.15;%K
-end
-    if indA == 1 && indB == 1
-        break
-    end
-end
-%Analyzing the S file and extracting the nexessary quantities
-filenameS = strcat(filename,'_s.csv');
-S = importdata(filenameS,',',4);
-W = strsplitter(S.textdata{1,1},',');
-colO2u = find(strncmpi(W,'ULO2',4));
-colCO2u = find(strncmpi(W,'ULCO2',5));
-colH2Ou = find(strncmpi(W,'ULH2O',5));
-colO2l = find(strncmpi(W,'LLO2',4));
-colCO2l = find(strncmpi(W,'LLCO2',5));
-colH2Ol = find(strncmpi(W,'LLH2O',5));
-dim = size(S.data);
-
-mfH2Ou = zeros(dim(1),numComp);
-mfCO2u = zeros(dim(1),numComp);
-mfO2u = zeros(dim(1),numComp);
-mfN2u = zeros(dim(1),numComp);
-MWAvgu = zeros(dim(1),numComp);
-YH2Ou = zeros(dim(1),numComp);
-YCO2u = zeros(dim(1),numComp);
-Rspecificu = zeros(a,numComp);
-mGASupper = zeros(a,numComp);
-mCO2expu = zeros(a,numComp);
-mH2Oexpu = zeros(a,numComp);
-
-mfH2Ol = zeros(dim(1),numComp);
-mfCO2l = zeros(dim(1),numComp);
-mfO2l = zeros(dim(1),numComp);
-mfN2l = zeros(dim(1),numComp);
-MWAvgl = zeros(dim(1),numComp);
-YH2Ol = zeros(dim(1),numComp);
-YCO2l = zeros(dim(1),numComp);
-Rspecificl = zeros(a,numComp);
-mGASlower = zeros(a,numComp);
-mCO2expl = zeros(a,numComp);
-mH2Oexpl = zeros(a,numComp);
-
-%upper layer mass fraction of H2O and CO2 calculations
-for i = 1:numComp
-for j = 1:dim(1)
-mfH2Ou(j,i) = S.data(j,colH2Ou(i));
-mfCO2u(j,i) = S.data(j,colCO2u(i));
-mfO2u(j,i) = S.data(j,colO2u(i));
-mfN2u(j,i) = 1 - (mfH2Ou(j,i)+mfCO2u(j,i)+mfO2u(j,i));
-MWAvgu(j,i) = mfH2Ou(j,i)*MH2O + mfCO2u(j,i)*MCO2 + mfO2u(j,i)*MO2 + mfN2u(j,i)*MN2;
-YH2Ou(j,i) = (mfH2Ou(j,i)*MH2O)/MWAvgu(j,i);
-YCO2u(j,i) = (mfCO2u(j,i)*MCO2)/MWAvgu(j,i);
-end
-end
-
-for i = 1:numComp
-    for j = 1:a;
-Rspecificu(j,i) = Cp*(gamma-1)/(gamma);%This formula calculates for the R value in the appropriate units (J/(kg*K))
-mGASupper(j,i) = P(j,i)*Vu(j,i)/(Rspecificu(j,i)*Tu(j,i));
-mCO2expu(j,i) = mGASupper(j,i) * YCO2u(j,i);
-mH2Oexpu(j,i) = mGASupper(j,i) * YH2Ou(j,i);
-    end
-end
-TotH2OmassU = sum(mH2Oexpu(a,:));
-TotCO2massU = sum(mCO2expu(a,:));
-
-%lower layer mass fraction of H2O and CO2 calculations
-for i = 1:numComp
-    if indA == 1 && indB == 1
-        break
-    end
-for j = 1:dim(1)
-mfH2Ol(j,i) = S.data(j,colH2Ol(i));
-mfCO2l(j,i) = S.data(j,colCO2l(i));
-mfO2l(j,i) = S.data(j,colO2l(i));
-mfN2l(j,i) = 1 - (mfH2Ol(j,i)+mfCO2l(j,i)+mfO2l(j,i));
-MWAvgl(j,i) = mfH2Ol(j,i)*MH2O + mfCO2l(j,i)*MCO2 + mfO2l(j,i)*MO2 + mfN2l(j,i)*MN2;
-YH2Ol(j,i) = (mfH2Ol(j,i)*MH2O)/MWAvgl(j,i);
-YCO2l(j,i) = (mfCO2l(j,i)*MCO2)/MWAvgl(j,i);
-end
-    if indA == 1 && indB == 1
-        break
-    end
-end
-
-for i = 1:numComp
-    if indA == 1 && indB == 1
-        break
-    end
-    for j = 1:a;
-Rspecificl(j,i) = Cp*(gamma-1)/(gamma);%This formula calculates for the R value in the appropriate units (J/(kg*K))
-mGASlower(j,i) = P(j,i)*Vl(j,i)/(Rspecificl(j,i)*Tl(j,i));
-mCO2expl(j,i) = mGASlower(j,i) * YCO2l(j,i);
-mH2Oexpl(j,i) = mGASlower(j,i) * YH2Ol(j,i);
-    end
-   if indA == 1 && indB == 1
-        break
-    end
-end
-
-
-TotH2OmassL = sum(mH2Oexpl(a,:));
-TotCO2massL = sum(mCO2expl(a,:));
-
-
-mCO2vector = mCO2expl + mCO2expu;%if multiple compartments are present, then this vector has multiple columns, total mass of CO2 in each compartment
-mH2Ovector = mH2Oexpl + mH2Oexpu;%if multiple compartments are present, then this vector has multiple columns, total mass of H2O in each compartment
-
-mCO2vectorFIN = sum(mCO2vector,2);%Total mass of CO2 for all compartments present
-mH2OvectorFIN = sum(mH2Ovector,2);%Total mass of H2O for all compartments present
-
-TotH2Omass = TotH2OmassL+TotH2OmassU;
-TotCO2mass = TotCO2massL+TotCO2massU;
 
 end

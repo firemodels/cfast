8a216ab2084b135a4a856ee16313791dedb591db
rpeacoc <rpeacoc@nist.gov>
2017-08-11 15:37:38 -0400

    Update version to 7.2.4

--- Source/Cedit/AssemblyInfo.vb
+++ Source/Cedit/AssemblyInfo.vb
@@ -29,4 +29,4 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("7.2.3.0")>
+<Assembly: AssemblyVersion("7.2.4.0")>

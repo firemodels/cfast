5dca0ecccfc3bd7e3c17296bbb92ce09335f9c4a
andytamhk168 <andytamhk168@yahoo.com>
2017-12-04 10:33:13 -0500

    Add radiation verification case, modifiy corresponding matlab and cfastbot scripts, and update latex files (CFASTBOT was ran)

--- Validation/scripts/CFAST_Cases.sh
+++ Validation/scripts/CFAST_Cases.sh
@@ -387,6 +387,7 @@ $RUNCFAST -d ../Verification/Sprinkler sprinkler_1.in
 
 $RUNCFAST -d ../Verification/Radiation radiation_1.in
 $RUNCFAST -d ../Verification/Radiation radiation_2.in
+$RUNCFAST -d ../Verification/Radiation radiation_3.in
 
 $RUNCFAST -d ../Verification/Fires Ignition_Test.in
 

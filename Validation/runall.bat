60228c2ae4406763601b36d69de1e4ecd9ef0735
rpeacoc <rpeacoc@gmail.com>
2018-03-28 14:59:04 -0400

    V&V: Add NIST/NRC Cabinet test 9

--- Validation/runall.bat
+++ Validation/runall.bat
@@ -309,6 +309,7 @@ background -u 98 ..\..\Utilities\for_bundle\Bin\cfast.exe cabinet_05.in -V
 background -u 98 ..\..\Utilities\for_bundle\Bin\cfast.exe cabinet_06.in -V
 background -u 98 ..\..\Utilities\for_bundle\Bin\cfast.exe cabinet_07.in -V
 background -u 98 ..\..\Utilities\for_bundle\Bin\cfast.exe cabinet_08.in -V
+background -u 98 ..\..\Utilities\for_bundle\Bin\cfast.exe cabinet_09.in -V
 cd ..\
 if %1==NIST_NRC_Corner_Effects goto end
 :NIST_Vent_Study

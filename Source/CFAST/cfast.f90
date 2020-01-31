df22bbaa4468be5cff7ebd86c0ce349a82c72872
rpeacoc <rpeacoc@gmail.com>
2020-01-31 13:55:48 -0500

    CFAST: Code formatting for consistency

--- Source/CFAST/cfast.f90
+++ Source/CFAST/cfast.f90
@@ -83,15 +83,15 @@
     if (.not.validation_flag) write (*,5010) total_steps
     write (iofill,5000) tend - tbeg
     write (iofill,5010) total_steps
-    
-    
+
+
     ! create the spreadsheet file of calculation results if necessary
-        if (n_dumps/=0.and.ss_out_interval/=0.) then
-            call output_spreadsheet_dump
-        else
-            call delete_output_files (sscalculation)
-        end if
-    
+    if (n_dumps/=0.and.ss_out_interval/=0.) then
+        call output_spreadsheet_dump
+    else
+        call delete_output_files (sscalculation)
+    end if
+
     call cfastexit ('CFAST', 0)
 
 5000 format ('Total execution time = ',1pg10.3,' seconds')

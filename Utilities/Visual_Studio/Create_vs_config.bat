f184b03238888667e746a3427cf2fba1bbdb41b0
Richard Peacock <rpeacoc@gmail.com>
2020-05-06 15:59:01 -0400

    CData: Initial commit of Monte Carlo generator

--- Utilities/Visual_Studio/Create_vs_config.bat
+++ Utilities/Visual_Studio/Create_vs_config.bat
@@ -1,4 +1,5 @@
 copy CFAST.sln ..\..\CFAST.sln /Y
 copy CFAST.vfproj ..\..\Source\CFAST\CFAST.vfproj /Y
 copy CEdit.vbproj ..\..\Source\CEdit\CEdit.vbproj /Y
+copy CData.vfproj ..\..\Source\CData\CData.vfproj /Y
 copy VandV_Calcs.vfproj ..\..\Source\VandV_Calcs\VandV_Calcs.vfproj /Y

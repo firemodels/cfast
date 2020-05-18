b1d695a9cd0aabced4a80e325faa3b7e86fd8e5d
Richard Peacock <rpeacoc@gmail.com>
2020-05-18 15:21:20 -0400

    CFAST: Eliminate unreferenced code

--- Source/CFAST/cfast.f90
+++ Source/CFAST/cfast.f90
@@ -26,13 +26,12 @@
 
     use precision_parameters
     
-    use initialization_routines, only : initialize_memory, initialize_fire_objects, initialize_species, initialize_walls
+    use initialization_routines, only : initialize_memory, initialize_species, initialize_walls
     use input_routines, only : open_files, read_input_file
-    use output_routines, only: output_version, output_initial_conditions, delete_output_files, closeoutputfiles
+    use output_routines, only: output_version, output_initial_conditions
     use solve_routines, only : solve_simulation
     use spreadsheet_routines, only : output_spreadsheet_dump
     use utility_routines, only : cptime, read_command_options
-    use radiation_routines, only : radiation
 
     use dump_data, only: n_dumps
     use option_data, only: total_steps

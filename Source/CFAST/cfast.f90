63f20a50770ccd4730a9cf66fdb4f04f464d37a5
Richard Peacock <rpeacoc@gmail.com>
2020-10-29 12:25:08 -0400

    CFAST: Update version number for release

--- Source/CFAST/cfast.f90
+++ Source/CFAST/cfast.f90
@@ -44,7 +44,7 @@
     real(eb) :: xdelt, tstop, tbeg, tend 
 
     program_name = 'CFAST'
-    cfast_version = 7601        ! Current CFAST version number
+    cfast_version = 7600        ! Current CFAST version number
 
     if (command_argument_count().eq.0) then
         call output_version(0)

838fc7664af738690cccda2dacb0f26ac0a55921
gforney <gforney@gmail.com>
2021-02-04 12:51:41 -0500

    sh2bat build: update sh2bat build script for new compilers

--- Utilities/sh2bat/make_sh2bat.bat
+++ Utilities/sh2bat/make_sh2bat.bat
@@ -1,6 +1,10 @@
 @echo off
 
-call "%IFORT_COMPILER16%\bin\compilervars" intel64
+:: setup compiler environment
+if x%arg1% == xbot goto skip1
+call ..\..\Build\scripts\setup_intel_compilers.bat intel64
+:skip1
+
 
 icl -o sh2bat sh2bat.c
 pause

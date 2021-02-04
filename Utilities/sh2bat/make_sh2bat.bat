be4c98ed06e7aa00c49a3cfd045882ddb542be58
gforney <gforney@gmail.com>
2021-02-04 12:53:30 -0500

    sh2bat build: add option to skip over pause statment"

--- Utilities/sh2bat/make_sh2bat.bat
+++ Utilities/sh2bat/make_sh2bat.bat
@@ -1,4 +1,5 @@
 @echo off
+set arg1=%1
 
 :: setup compiler environment
 if x%arg1% == xbot goto skip1
@@ -7,4 +8,7 @@ call ..\..\Build\scripts\setup_intel_compilers.bat intel64
 
 
 icl -o sh2bat sh2bat.c
+
+if x%arg1% == xnopause goto skip2
 pause
+:skip2

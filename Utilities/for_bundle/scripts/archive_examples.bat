917c170ecaf9197761254f0d608793975513375d
gforney <gforney@gmail.com>
2021-02-04 15:58:12 -0500

    archive scripts: correction to sh2bat to handle linux paths

--- Utilities/for_bundle/scripts/archive_examples.bat
+++ Utilities/for_bundle/scripts/archive_examples.bat
@@ -8,13 +8,14 @@ set sh2batdir=%cfastrepo%\Utilities\sh2bat
 set sh2batexe=%sh2batdir%\sh2bat.exe
 set copyCFASTcases=%cfastrepo%\Utilities\for_bundle\scripts\copyCFASTcases
 set validation_cases=%cfastrepo%\Validation\Scripts\CFAST_Cases.bat
+set verification_cases=%cfastrepo%\Verification\Scripts\CFAST_Cases.bat
 
 
 ::*** build sh2bat if it doesn't exist
 
 if exist %sh2batexe% goto skip_build_sh2bat
 cd %sh2batdir%
-call make_sh2bat
+call make_sh2bat nopause
 :skip_build_sh2bat
 
 echo sh2batexe=%sh2batexe%
@@ -45,5 +46,22 @@ set outdir=%CURDIR%\Examples\Validation
 
 cd %cfastrepo%\Validation
 call %validation_cases%
+
+::*** convert Verification case list
+
+:: comment out the following until the Veriffication CFAST_Cases.sh script is correct
+::cd %cfastrepo%\Verification\scripts
+::%sh2batexe% CFAST_Cases.sh CFAST_Cases.bat
+
+::*** copying Verification files
+::echo.
+::echo --- copying Verification files ---
+
+::set "RUNCFAST=call %copyCFASTcases%"
+::set outdir=%CURDIR%\Examples\Verification
+
+::cd %cfastrepo%\Verification
+::call %verification_cases%
+
 
 cd %CURDIR%
--- Verification/scripts/CFAST_Cases.sh
+++ Verification/scripts/CFAST_Cases.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-$RUNCFAST Miscellaneous cfast_test
-$RUNCFAST Mass_Energy_Balance Base
-$RUNCFAST Mass_Energy_Balance Added_Mass
+$RUNCFAST -d Miscellaneous       cfast_test
+$RUNCFAST -d Mass_Energy_Balance Base
+$RUNCFAST -d Mass_Energy_Balance Added_Mass

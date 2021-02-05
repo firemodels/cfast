a8d9eb75c2c15c58d8e725a63dc636c0db39e9d0
gforney <gforney@gmail.com>
2021-02-04 22:53:40 -0500

    bundle scripts: use sh2bat from a smokeview installation

--- Utilities/for_bundle/scripts/archive_examples.bat
+++ Utilities/for_bundle/scripts/archive_examples.bat
@@ -4,36 +4,24 @@ set CURDIR=%CD%
 
 cd ..\..\..
 set cfastrepo=%CD%
-set sh2batdir=%cfastrepo%\Utilities\sh2bat
-set sh2batexe=%sh2batdir%\sh2bat.exe
 set copyCFASTcases=%cfastrepo%\Utilities\for_bundle\scripts\copyCFASTcases
 set validation_cases=%cfastrepo%\Validation\Scripts\CFAST_Cases.bat
 
-::*** build sh2bat if it doesn't exist
-
-if exist %sh2batexe% goto skip_build_sh2bat
-cd %sh2batdir%
-call make_sh2bat nopause
-:skip_build_sh2bat
-
-echo sh2batexe=%sh2batexe%
-if exist %sh2batexe% goto skip_build_sh2bat2
-echo %sh2batexe% does not exist
-echo copy examples script abored
-exit
-:skip_build_sh2bat2
+:: make sure sh2bat is installed
+call :is_installed sh2bat
 
 ::*** create directories
 
 cd %CURDIR%
-if not exist Examples              mkdir Examples
-if not exist Examples\Verification mkdir Examples\Verification
-if not exist Examples\Validation   mkdir Examples\Validation
+if exist Examples rmdir /s /q Examples
+mkdir Examples
+mkdir Examples\Verification
+mkdir Examples\Validation
 
 ::*** convert Validation case list
 
 cd %cfastrepo%\Validation\scripts
-%sh2batexe% CFAST_Cases.sh CFAST_Cases.bat
+sh2bat CFAST_Cases.sh CFAST_Cases.bat
 
 ::*** copying Validation files
 echo.
@@ -46,3 +34,22 @@ cd %cfastrepo%\Validation
 call %validation_cases%
 
 cd %CURDIR%
+goto eof
+
+:: -------------------------------------------------------------
+:is_file_installed
+:: -------------------------------------------------------------
+
+  set program=%1
+  %program% --help 1>> is_installed.txt 2>&1
+  type is_installed.txt | find /i /c "not recognized" > error_count.txt
+  set /p nothave=<error_count.txt
+  if %nothave% == 1 (
+    echo "***Fatal error: %program% not present"
+    echo "installer aborted"
+    erase is_installed.txt error_count.txt
+    exit /b 1
+  )
+  exit /b 0
+
+:eof

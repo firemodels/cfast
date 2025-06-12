ff0e7e9bb587601bcae22157bc5c10c7ee435bbc
gforney <gforney@gmail.com>
2025-06-11 20:38:34 -0400

    cfast source: change mdot_pyrolysis_burned back to mdot_pyrolysis

--- Source/CFAST/fire.f90
+++ Source/CFAST/fire.f90
@@ -68,33 +68,22 @@ module fire_routines
         call interpolate_pyrolysis(i,tsec,iroom,mdot_t,area_t,height_t,qdot_t,hoc_t,n_C,n_H,n_O,n_N,n_Cl,y_soot,y_soot_flaming, &
             y_soot_smolder,y_co,y_hcn,y_trace)
 
-#ifdef pp_FIRE
-        fireptr%mdot_pyrolysis_burned = mdot_t
-#else
-        fireptr%mdot_pyrolysis = mdot_t
-#endif
+        fireptr%mdot_pyrolysis   = mdot_t
         fireptr%qdot_theoretical = qdot_t
         
         fireptr%z_offset = height_t
         species_mass(u,1:ns) = roomptr%species_mass(u,1:ns)
         species_mass(l,1:ns) = roomptr%species_mass(l,1:ns)
 
-#ifdef pp_FIRE
-        call do_fire(i, iroom, fireptr%mdot_pyrolysis_burned, roomptr%cheight, roomptr%cwidth, roomptr%cdepth, hoc_t, y_soot, &
-            y_soot_flaming, y_soot_smolder, y_co, y_hcn, y_trace, n_C, n_H, n_O, n_N, n_Cl, fireptr%molar_mass, species_mass, &
-            fireptr%x_position, fireptr%y_position, fireptr%z_position+fireptr%z_offset, area_t, fireptr%mdot_entrained, &
-            fireptr%mdot_plume, qdot_t, species_mass_rate, hrr_c, hrr_r, fireptr%qdot_layers(l), fireptr%qdot_layers(u))
-        mdot_pyrolysis_unburned = mdot_t - fireptr%mdot_pyrolysis_burned
-        fireptr%firearea = area_t
-        fireptr%mdot_trace = fireptr%mdot_pyrolysis_burned*y_trace
-#else
         call do_fire(i, iroom, fireptr%mdot_pyrolysis, roomptr%cheight, roomptr%cwidth, roomptr%cdepth, hoc_t, y_soot, &
             y_soot_flaming, y_soot_smolder, y_co, y_hcn, y_trace, n_C, n_H, n_O, n_N, n_Cl, fireptr%molar_mass, species_mass, &
             fireptr%x_position, fireptr%y_position, fireptr%z_position+fireptr%z_offset, area_t, fireptr%mdot_entrained, &
             fireptr%mdot_plume, qdot_t, species_mass_rate, hrr_c, hrr_r, fireptr%qdot_layers(l), fireptr%qdot_layers(u))
+#ifdef pp_FIRE
+        mdot_pyrolysis_unburned = mdot_t - fireptr%mdot_pyrolysis
+#endif
         fireptr%firearea = area_t
         fireptr%mdot_trace = fireptr%mdot_pyrolysis*y_trace
-#endif
         fireptr%qdot_actual = fireptr%qdot_layers(l) + fireptr%qdot_layers(u)
         fireptr%qdot_convective = hrr_c
         fireptr%qdot_radiative = hrr_r
@@ -111,19 +100,16 @@ module fire_routines
         ! sum the flows for return to the source routine
 
         #ifdef pp_FIRE
-! hrr_c mass flow into upper layer - mdot_pyrolysis_burned
+! hrr_c mass flow into upper layer - mdot_pyrolysis
 ! q_firemass flow into upper layer - mdot_pyrolysis_unburned
-! so total mass flow into upper layer is mdot_pyrolysis_burned + mdot_pyrolysis_unburned = mdot_t
+! so total mass flow into upper layer is mdot_pyrolysis + mdot_pyrolysis_unburned = mdot_t
         flows_fires(iroom,m,u) = flows_fires(iroom,m,u) + mdot_t             + fireptr%mdot_entrained
-        flows_fires(iroom,m,l) = flows_fires(iroom,m,l)                      - fireptr%mdot_entrained
-        flows_fires(iroom,q,u) = flows_fires(iroom,q,u) + hrr_c + q_firemass + q_entrained
-        flows_fires(iroom,q,l) = flows_fires(iroom,q,l)                      - q_entrained
 #else
         flows_fires(iroom,m,u) = flows_fires(iroom,m,u) + fireptr%mdot_plume
+#endif
         flows_fires(iroom,m,l) = flows_fires(iroom,m,l) - fireptr%mdot_entrained
         flows_fires(iroom,q,u) = flows_fires(iroom,q,u) + hrr_c + q_firemass + q_entrained
         flows_fires(iroom,q,l) = flows_fires(iroom,q,l) - q_entrained
-#endif
         flows_fires(iroom,3:ns+2,u) = flows_fires(iroom,3:ns+2,u) + species_mass_rate(u,1:ns)
         flows_fires(iroom,3:ns+2,l) = flows_fires(iroom,3:ns+2,l) + species_mass_rate(l,1:ns)
 !        write(6,10)tsec,flows_fires(iroom,q,u)+flows_fires(iroom,q,l)
@@ -664,11 +650,7 @@ module fire_routines
 
     do i = 1, n_fires
         fireptr => fireinfo(i)
-#ifdef pp_FIRE
-        fireptr%total_pyrolysate = fireptr%total_pyrolysate + fireptr%mdot_pyrolysis_burned*deltt
-#else
         fireptr%total_pyrolysate = fireptr%total_pyrolysate + fireptr%mdot_pyrolysis*deltt
-#endif
         fireptr%total_trace = fireptr%total_trace + fireptr%mdot_trace*deltt
     end do
 

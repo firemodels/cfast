a488c230b6eb964c9c51de44070bd8ea29470dde
mcgratta <mcgratta@gmail.com>
2026-04-16 10:57:20 -0400

    Convert VandV_Calcs to Python

--- Build/VandV_Calcs/gnu_linux/make_vv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-platform=gnu_linux
-dir=`pwd`
-target=${dir##*/}
-
-echo Building $target
-make -f ../makefile $target
--- Build/VandV_Calcs/gnu_osx/make_vv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-platform=gnu_osx
-dir=`pwd`
-target=${dir##*/}
-
-echo Building $target
-make -f ../makefile $target
